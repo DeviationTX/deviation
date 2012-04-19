fdd0e1e023728548ca5b8a5b70316967fe2630ee
PhracturedBlue <rc2012@pblue.org>
2012-04-19 06:39:52 -0700

    Add license info

--- main.c
+++ main.c
@@ -1,6 +1,18 @@
-//###############################
-//Multi-Inno MIO283QT-2 (HX8347D)
-//###############################
+/*
+    This project is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Foobar is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include <libopencm3/stm32/f1/flash.h>
 
 #include "tx.h"
