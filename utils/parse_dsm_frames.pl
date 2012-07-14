964cd5e7c64b0eaaf6fec9f8fbf09cb999247b75
PhracturedBlue <rc2012@pblue.org>
2012-07-13 20:02:42 -0700

    Add some data analysis scripts

---
+++ utils/parse_dsm_frames.pl
@@ -0,0 +1,57 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use Getopt::Long;
+my $all = 0;
+my $starttime = 0;
+my $telem = 1;
+my $mux = 0;
+GetOptions("mux"=> \$mux, "all" => \$all, "time=f" => \$starttime);
+$_ = <>;
+my $ok = 0;
+my $last_frame = -1;
+my $cmd = 0;
+my $start = 0;
+my $msb_first = 0;
+my @data = ();
+
+while(<>) {
+    my($time, $framenum, $data, $din) = (/^(\S+),(\S+),(\S+),(\S*)/);
+    next unless($time && $framenum && $data);
+    $time -= $starttime;
+    $data = hex($data);
+    $din = hex($din);
+    if($framenum != $last_frame) {
+        if(@data) {
+            if(($data & 0x3f) >= 0x20 && ($data & 0x3f) <= 0x25) {
+                if (sprintf("%02x", $data) eq $data[0]) {
+                    $last_frame = $framenum;
+                    #$msb_first = 1;
+                    next;
+                }
+            }
+            printf("%15.6f%s @data\n", $start, $ok == 1 ? ":" : "#");
+        }
+        @data = ();
+        $last_frame = $framenum;
+        $start = $time;
+        if ($all) {
+            $ok = ($data & 0x80) ? 1 : 2;
+        } else {
+            if ( grep {($data & 0x3f) == $_} (0x00, 0x20, 0x21, 0x22, 0x23, 0x24, 0x25)) {
+                $ok = ($data & 0x80) ? 1 : 2;
+            } else {
+                $ok = 0;
+            }
+        }
+    }
+    if ($ok) {
+        my $d = (! @data || ($ok == 1 || $mux)) ? $data : $din;
+        if($msb_first) {
+            splice @data, 1, 0, sprintf("%02x", $d);
+            $msb_first = 0;
+        } else {
+            push @data, sprintf("%02x", $d);
+        }
+    }
+}
---
+++ utils/parse_v911_frames.pl
@@ -0,0 +1,38 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+my $start = shift(@ARGV);
+my $found = 0;
+my $last_frame = 0;
+my $frame_start = 0;
+my @data = ();
+my $lsb = 1;
+my $tmp = 0;
+$_ = <>;
+while(<>) {
+    chomp;
+    my($time, $framenum, $data) = split(/,/, $_);
+    $data = hex($data);
+    $found = 1 if(int($framenum) == $start);
+    next unless($found);
+    if($framenum > $last_frame + 1) {
+        $last_frame = $framenum;
+        $frame_start = $time;
+        @data = ();
+    }
+    if($framenum == $last_frame) {
+        if($lsb) {
+            $tmp = $data;
+            $lsb = 0;
+        } else {
+            push @data, sprintf("%04x", ($data << 8) + $tmp);
+            $lsb = 1;
+        }
+    } else {
+        $_ = <>;
+        ($time, $framenum, $data) = split(/,/, $_);
+        my $channel = hex($data);
+        printf("%15s(Channel: %02x), @data\n", $frame_start, $channel);
+    }
+}
