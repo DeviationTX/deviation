b8ea5ed3045e42a9154772c282310eb1079abaa9
PhracturedBlue <rc2012@pblue.org>
2012-08-08 13:11:09 -0700

    Reverse rudder

--- src/target/devo8/channels.c
+++ src/target/devo8/channels.c
@@ -105,7 +105,7 @@ s16 CHAN_ReadInput(int channel)
     } else {
         value = value ? CHAN_MAX_VALUE : CHAN_MIN_VALUE;
     }
-    if (channel == INP_THROTTLE)
+    if (channel == INP_THROTTLE || channel == INP_RUDDER)
         value = -value;
     return value;
 }
