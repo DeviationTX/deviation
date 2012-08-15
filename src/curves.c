10febb4110780b281203f4083d8910f8bdb44e45
PhracturedBlue <rc2012@pblue.org>
2012-08-15 07:39:26 -0700

    'Fixed' now is adjusted by 'Scale' rather than 'offset' so it is accessible from the Expo page

--- src/curves.c
+++ src/curves.c
@@ -72,7 +72,7 @@ s16 CURVE_Evaluate(s16 xval, struct Curve *curve)
 {
     switch (curve->type) {
         case CURVE_NONE:     return xval;
-        case CURVE_FIXED:    return 0;
+        case CURVE_FIXED:    return CHAN_MAX_VALUE;
         case CURVE_MIN_MAX:  return (xval < 0) ? CHAN_MIN_VALUE : CHAN_MAX_VALUE;
         case CURVE_ZERO_MAX: return (xval < 0) ? 0 : CHAN_MAX_VALUE;
         case CURVE_GT_ZERO:  return (xval < 0) ? 0 : xval;
