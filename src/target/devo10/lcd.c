4d604be852310ca41049f7c7effdacd13c135a84
PhracturedBlue <rc2012@pblue.org>
2012-09-09 21:24:57 -0700

    Working LCD based on xCometz's work

--- src/target/devo10/lcd.c
+++ src/target/devo10/lcd.c
@@ -17,71 +17,51 @@
 #include <libopencm3/stm32/fsmc.h>
 #include "common.h"
 
-#define LCD_REG_ADDR  ((uint32_t)FSMC_BANK1_BASE)    /* Register Address */
-#define LCD_DATA_ADDR  ((uint32_t)FSMC_BANK1_BASE + 0x20000)  /* Data Address */
+#define LCD_CMD_ADDR ((uint32_t)FSMC_BANK1_BASE) /* Register Address */
+#define LCD_DATA_ADDR ((uint32_t)FSMC_BANK1_BASE + 0x10000) /* Data Address */
 
-#define LCD_REG  *(volatile uint16_t *)(LCD_REG_ADDR)
-#define LCD_DATA *(volatile uint16_t *)(LCD_DATA_ADDR)
+#define LCD_CMD *(volatile uint8_t *)(LCD_CMD_ADDR)
+#define LCD_DATA *(volatile uint8_t *)(LCD_DATA_ADDR)
 
-static u8 invert;
-void lcd_cmd(uint8_t addr, uint8_t data)
+#define LCD_WIDTH 129
+#define LCD_PAGES 8
+static u8 img[LCD_WIDTH * LCD_PAGES];
+static u8 dirty[LCD_WIDTH];
+static u8 xstart, xend;
+static u8 xpos, ypos;
+static enum DrawDir dir;
+
+void lcd_display(uint8_t on)
 {
-    LCD_REG = addr;
-    LCD_DATA = data;
+    LCD_CMD = 0xAE | (on ? 1 : 0);
 }
 
-void lcd_set_pos(unsigned int x0, unsigned int y0)
+void lcd_write_display_data(uint8_t display_data)
 {
-    if (invert)
-        y0 = 239 - y0;
-    //printf("lcd_set_pos: %d, %d\n", x0, y0);
-    lcd_cmd(0x03, (x0>>0)); //set x0
-    lcd_cmd(0x02, (x0>>8)); //set x0
-    lcd_cmd(0x07, (y0>>0)); //set y0
-    lcd_cmd(0x06, (y0>>8)); //set y0
-    LCD_REG = 0x22;
+    LCD_DATA = display_data;
 }
 
-void LCD_DrawPixel(unsigned int color)
+void lcd_set_page_address(uint8_t page)
 {
-    LCD_DATA = color;
+    LCD_CMD = 0xB0 | (page & 0x07);
 }
 
-void LCD_DrawPixelXY(unsigned int x, unsigned int y, unsigned int color)
+void lcd_set_column_address(uint8_t column)
 {
-    lcd_set_pos(x, y);
-    LCD_DATA = color;
+    LCD_CMD = 0x10 | ((column >> 4) & 0x0F);  //MSB
+    LCD_CMD = column & 0x0F;                  //LSB
 }
 
-void LCD_DrawStart(unsigned int x0, unsigned int y0, unsigned int x1, unsigned int y1, enum DrawDir dir)
+void lcd_set_start_line(int line)
 {
-  if (dir == DRAW_SWNE) {
-    unsigned int y = 239 - y0;
-    y0 = 239 - y1;
-    y1 = y;
-    invert = 1;
-    lcd_cmd(0x16, 0x28); //MY=0 MX=0 MV=1 ML=0 BGR=1
-  } else {
-    invert = 0;
-    lcd_cmd(0x16, 0x68); //MY=0 MX=1 MV=1 ML=0 BGR=1
-  }
-  //printf("LCD_DrawStart: (%d, %d) - (%d, %d)\n", x0, y0, x1, y1);
-  lcd_cmd(0x03, (x0>>0)); //set x0
-  lcd_cmd(0x02, (x0>>8)); //set x0
-  lcd_cmd(0x05, (x1>>0)); //set x1
-  lcd_cmd(0x04, (x1>>8)); //set x1
-  lcd_cmd(0x07, (y0>>0)); //set y0
-  lcd_cmd(0x06, (y0>>8)); //set y0
-  lcd_cmd(0x09, (y1>>0)); //set y1
-  lcd_cmd(0x08, (y1>>8)); //set y1
-  LCD_REG = 0x22;
-
-  return;
+  LCD_CMD = (line & 0x3F) | 0x40; 
 }
 
-void LCD_DrawStop(void)
+//Electronic Volume Control
+void lcd_set_brightness(int b)
 {
-  return;
+  LCD_CMD = 0x81;
+  LCD_CMD = b & 0x3F;
 }
 
 void LCD_Init()
@@ -92,92 +72,148 @@ void LCD_Init()
 
     gpio_set_mode(GPIOD, GPIO_MODE_OUTPUT_50_MHZ,
                   GPIO_CNF_OUTPUT_ALTFN_PUSHPULL,
-                  GPIO0 | GPIO1 | GPIO8 | GPIO9 | GPIO10 | GPIO14 | GPIO15);
+                  GPIO0 | GPIO1 | GPIO10 | GPIO14 | GPIO15); //D2|D3|D0|D1
 
     gpio_set_mode(GPIOE, GPIO_MODE_OUTPUT_50_MHZ,
                   GPIO_CNF_OUTPUT_ALTFN_PUSHPULL,
-                  GPIO7 | GPIO8 | GPIO9 | GPIO10 | GPIO11 | GPIO12 | GPIO13 | GPIO14 | GPIO15);
+                  GPIO7 | GPIO8 | GPIO9 | GPIO10); // D4|D5|D6|D7
 
-    gpio_set_mode(GPIOD, GPIO_MODE_OUTPUT_50_MHZ,
+    gpio_set_mode(GPIOD, GPIO_MODE_OUTPUT_50_MHZ, // A16
                   GPIO_CNF_OUTPUT_ALTFN_PUSHPULL,
                   GPIO11);
 
-    gpio_set_mode(GPIOD, GPIO_MODE_OUTPUT_50_MHZ,
+    gpio_set_mode(GPIOD, GPIO_MODE_OUTPUT_50_MHZ, // NOE|NWE
                   GPIO_CNF_OUTPUT_ALTFN_PUSHPULL,
                   GPIO4 | GPIO5);
 
-    gpio_set_mode(GPIOD, GPIO_MODE_OUTPUT_50_MHZ,
+    gpio_set_mode(GPIOD, GPIO_MODE_OUTPUT_50_MHZ, // NE1 (not connectred)
                   GPIO_CNF_OUTPUT_ALTFN_PUSHPULL,
                   GPIO7);
 
-    /* Extended mode, write enable, 16 bit access, bank enabled */
-    FSMC_BCR1 = FSMC_BCR_MWID | FSMC_BCR_WREN | FSMC_BCR_MBKEN;
+    /* Normal mode, write enable, 8 bit access, SRAM, bank enabled */
+    FSMC_BCR1 = (FSMC_BCR1 & 0x000000C0) | FSMC_BCR_WREN | FSMC_BCR_MBKEN;
 
     /* Read & write timings */
-    FSMC_BTR1  = FSMC_BTR_DATASTx(2) | FSMC_BTR_ADDHLDx(0) | FSMC_BTR_ADDSETx(1) | FSMC_BTR_ACCMODx(FSMC_BTx_ACCMOD_B);
-    FSMC_BWTR1 = FSMC_BTR_DATASTx(2) | FSMC_BTR_ADDHLDx(0) | FSMC_BTR_ADDSETx(1) | FSMC_BTR_ACCMODx(FSMC_BTx_ACCMOD_B);
+    /* Data Setup > 90ns, Address Setup = 2xHCLK to ensure no output collision in 6800
+       mode since LCD E and !CS always active */
+    FSMC_BTR1 = FSMC_BTR_DATASTx(7) | FSMC_BTR_ADDHLDx(0) | FSMC_BTR_ADDSETx(2);
 
-  //driving ability
-  lcd_cmd(0xEA, 0x00);
-  lcd_cmd(0xEB, 0x20);
-  lcd_cmd(0xEC, 0x0C);
-  lcd_cmd(0xED, 0xC4);
-  lcd_cmd(0xE8, 0x40);
-  lcd_cmd(0xE9, 0x38);
-  lcd_cmd(0xF1, 0x01);
-  lcd_cmd(0xF2, 0x10);
-  lcd_cmd(0x27, 0xA3);
+    // LCD bias setting (11); 0xA2; 1/9
+    LCD_CMD = 0xA2;
 
-  //power voltage
-  lcd_cmd(0x1B, 0x1B);
-  lcd_cmd(0x1A, 0x01);
-  lcd_cmd(0x24, 0x2F);
-  lcd_cmd(0x25, 0x57);
+    // ADC selection (8) -> ADdressCounter; 0xA0; inc
+    LCD_CMD = 0xA0;
 
-  //VCOM offset
-  lcd_cmd(0x23, 0x8D); //for flicker adjust
+    //Common output mode selection (15); 0xC0; normal scan
+    LCD_CMD = 0xC0;
+    Delay(5);
 
-//ownTry
-//lcd_cmd(0x29,0x06);
-//lcd_cmd(0x2A,0x00);
-//lcd_cmd(0x2C,0x06);
-//lcd_cmd(0x2D,0x06);
+    //Setting built-in resistance ratio (17); 0x24; default=7.50
+    LCD_CMD = 0x24;
 
-  //power on
-  lcd_cmd(0x18, 0x36);
-  lcd_cmd(0x19, 0x01); //start osc
-  lcd_cmd(0x01, 0x00); //wakeup
-  lcd_cmd(0x1F, 0x88);
-  Delay(5);
-  lcd_cmd(0x1F, 0x80);
-  Delay(5);
-  lcd_cmd(0x1F, 0x90);
-  Delay(5);
-  lcd_cmd(0x1F, 0xD0);
-  Delay(5);
+    //Electronic volume control (18) -> LCD brightness; 0x20; default=32d
+    lcd_set_brightness(0x25);
+    Delay(5);
 
-  lcd_cmd(0x16, 0x68); //MY=0 MX=1 MV=1 ML=0 BGR=1
+    //Power control setting (16); V/B, V/R, V/F are used
+    LCD_CMD = 0x2F;
+    Delay(5);
 
-  //color selection
-  lcd_cmd(0x17, 0x05); //0x0005=65k, 0x0006=262k
+    // Read-Modify-Write (12); let read data does not increment column address
+    LCD_CMD = 0xE0;
 
-  //panel characteristic
-  lcd_cmd(0x36, 0x00);
-
-  //display on
-  lcd_cmd(0x28, 0x38);
-  Delay(40);
-  lcd_cmd(0x28, 0x3C);
+    lcd_set_start_line(0);
+    lcd_set_page_address(0);
+    lcd_set_column_address(0);
+    // Display data write (6)
+    lcd_display(1);
+    memset(img, 0, sizeof(img));
+    memset(dirty, 0, sizeof(dirty));
+ 
 }
 
-void LCD_Sleep()
+
+void LCD_Clear(unsigned int val)
 {
-    lcd_cmd(0x28, 0x38);
-    Delay(40);
-    lcd_cmd(0x1f, 0x89);
-    Delay(40);
-    lcd_cmd(0x28, 0x04);
-    Delay(40);
-    lcd_cmd(0x19, 0x00);
-    Delay(5);
+    int i,j;
+    val = (val & 0xFF);
+    for (i=0; i<LCD_PAGES; i++) {
+        lcd_set_page_address(i);
+        lcd_set_column_address(0);
+        for (j=0;j<=LCD_WIDTH;j++)
+            LCD_DATA = val;
+    }
+}
+
+void LCD_DrawStart(unsigned int x0, unsigned int y0, unsigned int x1, unsigned int y1, enum DrawDir _dir)
+{
+    (void)y1;
+    dir = _dir;
+    xstart = x0;
+    xend = x1;
+    xpos = x0;
+    ypos = y0;
+}
+/* Screen coordinates are as follows:
+ * (128, 32)   ....   (0, 32)
+ *   ...       ....     ...
+ * (128, 63)   ....   (0, 63)
+ * (128, 0)    ....   (0, 0)
+ *   ...       ....     ...
+ * (128, 31)   ....   (0, 31)
+ */
+void LCD_DrawStop(void)
+{
+    int col = 0;
+    int p, c;
+    for (p = 0; p < LCD_PAGES; p++) {
+        int init = 0;
+        for (c = 0; c < LCD_WIDTH; c++) {
+            if(dirty[c] & (1 << p)) {
+                if(! init) {
+                    lcd_set_page_address(p);
+                    lcd_set_column_address(c);
+                } else if(col+1 != c) {
+                    lcd_set_column_address(c);
+                }
+                LCD_DATA = img[p * LCD_WIDTH + c];
+                col = c;
+            }
+        }
+    }
+    memset(dirty, 0, sizeof(dirty));
+}
+
+void LCD_DrawPixel(unsigned int color)
+{
+    int y = ypos;
+    int x = 128 - xpos;
+
+    if (ypos > 31)
+        y = ypos - 32;
+    else
+        y = ypos + 32;
+    if (dir == DRAW_SWNE)
+        y = 63 - y;
+    
+    int ycol = y / 8;
+    int ybit = y & 0x07;
+    if(color) {
+        img[ycol * LCD_WIDTH + x] |= 1 << ybit;
+    } else {
+        img[ycol * LCD_WIDTH + x] &= ~(1 << ybit);
+    }
+    dirty[x] |= 1 << ycol;
+    xpos++;
+    if (xpos > xend) {
+        xpos = xstart;
+        ypos++;
+    }
+}
+
+void LCD_DrawPixelXY(unsigned int x, unsigned int y, unsigned int color)
+{
+    xpos = x;
+    ypos = y;
+    LCD_DrawPixel(color);
 }
