9a69ab5bbb96ca9df63d6c08bbcad098f57fda4c
PhracturedBlue <rc2012@pblue.org>
2012-11-12 10:14:19 -0800

    Create language files specific to each Tx

--- utils/extract_strings.pl
+++ utils/extract_strings.pl
@@ -5,18 +5,62 @@ use warnings;
 use Getopt::Long;
 my $update;
 my $lang;
+my $target;
+my @targets = ("320x240x16", "128x64x1");
+my %targetmap = (
+    devo8 => "320x240x16",
+    devo10 => "128x64x1",
+);
 
-GetOptions("update" => \$update, "language=s" => \$lang);
-my @files = `find . -name "*.[hc]" | xargs xgettext -o - --omit-header -k --keyword=_tr --keyword=_tr_noop --no-wrap`;
-my @out;
-foreach (@files) {
+GetOptions("update" => \$update, "language=s" => \$lang, "target=s" => \$target);
+if($target && (! $targetmap{$target} || ! (grep {$targetmap{$target} eq $_} @targets))) {
+    my @t = keys(%targetmap);
+    print "Target must be one of: @t\n";
+    exit 1;
+}
+my @lines = `find . -name "*.[hc]" | xargs xgettext -o - --omit-header -k --keyword=_tr --keyword=_tr_noop --no-wrap`;
+my @files;
+my $str = "";
+my $idx = 0;
+my %filemap;
+#Read all strings and put into a hash that maps the containing file to the string
+foreach (@lines) {
+    if(/^\s*$/ || /^msgstr/) {
+        if($str) {
+            foreach my $f (@files) {
+                $filemap{$f} = $str;
+            }
+        }
+        @files = ();
+        $str = "";
+        $idx++;
+    }
+    if(/#:\s+(\S+):\d+$/) {
+        push @files, "$1:$idx";
+    }
     next unless(/^(msgid|")/);
     if(/^msgid/) {
-        push @out, "";
+        $str = "";
     }
     s/^.*?"(.*)"\s*$/$1/;
-    $out[-1] .= $_;
+    $str .= $_;
 }
+if($str) {
+    foreach my $f (@files) {
+        $filemap{$f} = $str;
+    }
+}
+#Filter out any files that are not used by the specified target
+if($target) {
+    foreach my $f (keys %filemap) {
+        if(grep {$_ ne $targetmap{$target} && $f =~ /$_/} @targets) {
+            delete $filemap{$f};
+        }
+    }
+}
+#build string list
+my@out = values(%filemap);
+#append template names
 foreach (`head -n 1 filesystem/common/template/*.ini`) {
     chomp;
     if(/template=(.*?)\s*$/) {
@@ -25,7 +69,7 @@ foreach (`head -n 1 filesystem/common/template/*.ini`) {
 }
 my %uniq = map {$_ => undef} @out;
 
-if(! $update) {
+if(! $update && ! $target) {
     foreach (sort keys %uniq) {
         print ":$_\n";
     }
@@ -35,6 +79,7 @@ if(! $update) {
 @files = glob($lang ?
               "filesystem/common/language/lang*.$lang"
               : "filesystem/common/language/lang*");
+
 foreach my $file (@files) {
     my %strings = %uniq;
     my %unused;
@@ -53,18 +98,32 @@ foreach my $file (@files) {
             $strings{$eng} = $next;
         }
     }
-    open $fh, ">", $file;
-    print $fh $name;
-    foreach (sort keys %strings) {
-        if ($unused{$_}) {
-            print $fh "<:$_\n<$strings{$_}\n";
-        } elsif(! defined($strings{$_})) {
-            print $fh ">:$_\n";
-        } else {
-            print $fh ":$_\n$strings{$_}\n";
+    if($target) {
+        #if target is specified, we want to return a filtered list of strings
+        my $outf = $file;
+        $outf =~ s/common/$target/;
+        open $fh, ">", $outf;
+        print $fh $name;
+        foreach (sort keys %strings) {
+            if(! $unused{$_} && defined($strings{$_})) {
+                print $fh ":$_\n$strings{$_}\n";
+            }
         }
+        close $fh;
+    } else {
+        open $fh, ">", $file;
+        print $fh $name;
+        foreach (sort keys %strings) {
+            if ($unused{$_}) {
+                print $fh "<:$_\n<$strings{$_}\n";
+            } elsif(! defined($strings{$_})) {
+                print $fh ">:$_\n";
+            } else {
+                print $fh ":$_\n$strings{$_}\n";
+            }
+        }
+        close $fh;
     }
-    close $fh;
 }
 
 # | xargs xgettext -o - --omit-header -k --keyword=_tr --keyword=_tr_noop --no-wrap | grep -v 'msgstr ""' | grep -v "^#" | grep -v "^$"
