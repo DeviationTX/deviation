888f2aab50b5c826d52bc6d9a060ca1e1fa643c8
PhracturedBlue <rc2012@pblue.org>
2013-01-18 21:07:10 -0800

    Replace printf/fprintf/sprintf implementation

--- src/std.h
+++ src/std.h
@@ -3,17 +3,26 @@
 
 /* the following defines allow control over how stdio/stdlib functions are handles */
 #ifndef EMULATOR
+
+#include <stdarg.h>
+void tfp_printf(char *fmt, ...);
+void tfp_sprintf(char* s,char *fmt, ...);
+void tfp_fprintf(FILE *fh,char *fmt, ...);
+
 #if (! defined BUILDTYPE_DEV)
   //Use this instead of printf(args...) because this will avoid
   //compile warnings
   #define printf if(0) iprintf
 #else
-  #define printf iprintf
+  //#define printf iprintf
+  #define printf tfp_printf
 #endif
 
-#define sprintf siprintf
+//#define sprintf siprintf
+#define sprintf tfp_sprintf
 #define snprintf sniprintf
-#define fprintf fiprintf
+//#define fprintf fiprintf
+#define fprintf tfp_fprintf
 //#define fread(a, b, c, d) 0
 //#define fopen(a, b) -1
 //#define fclose if(0) fclose
