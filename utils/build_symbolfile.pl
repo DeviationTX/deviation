f12d6be4f62e2829af0a4a10905677b26344cf3a
PhracturedBlue <rc2012@pblue.org>
2013-01-20 10:35:05 -0800

    Add missing build_symbolfile.pl script

---
+++ utils/build_symbolfile.pl
@@ -0,0 +1,8 @@
+#!/usr/bin/perl
+while(<>) {
+	if(/^(\S+) g.....([FO])..* (\S+)/) {
+            printf "$3 = 0x%08x;\n", hex($1) + ($2 eq "O" ? 0 : 1);
+        } elsif(/^(\S+).*\*ABS\*\s+0+\s+_data_loadaddr/) {
+            printf "_data_loadaddr = 0x%08x;\n", hex($1);
+        }
+}
