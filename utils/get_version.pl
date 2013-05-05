1bb16e59e80204493601fe1e5e16b7a6ce9f32ea
PhracturedBlue <rc2012@pblue.org>
2013-05-05 14:01:34 -0700

    Identify version by previous release.  Fixes #332

---
+++ utils/get_version.pl
@@ -0,0 +1,36 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+my $target = shift (@ARGV);
+my $fh;
+if (! open $fh, "hg log -f --template 'Version: {node} Tags: {tags}\n' 2> /dev/null |") {
+    print "${target}-Unknown";
+    exit 0;
+}
+my $count = 0;
+my $tag;
+my $version;
+while(<$fh>) {
+    chomp;
+    if(! /Version:\s+(\S+)\s+Tags:\s+(.*)/) {
+        next;
+    }
+    $version ||= $1;
+    if($2 && $2 ne "tip") {
+        $tag = $2;
+        last;
+    }
+    $count++;
+}
+if(! $version) {
+    print "${target}-Unknown";
+} elsif(! $tag) {
+    print "${target}-$version";
+} elsif ($count == 1) {
+    print "${target}-$tag";
+} else {
+    print "${target}-$tag-" . substr($version, 0, 6);
+}
+exit 0;
+
