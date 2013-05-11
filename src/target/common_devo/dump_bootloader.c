740d1d025c264fa7868aa7de51167cc445fd631c
PhracturedBlue <rc2012@pblue.org>
2013-05-11 08:13:43 -0700

    Update bootloader dumper

--- src/target/common_devo/dump_bootloader.c
+++ src/target/common_devo/dump_bootloader.c
@@ -6,19 +6,19 @@ void dump_bootloader()
 
     printf("Erase...\n");
 
-    SPIFlash_EraseSector(0x2000);
-    SPIFlash_EraseSector(0x3000);
-    SPIFlash_EraseSector(0x4000);
-    SPIFlash_EraseSector(0x5000);
+    SPIFlash_EraseSector(0x1000*SPIFLASH_SECTOR_OFFSET + 0x0000);
+    SPIFlash_EraseSector(0x1000*SPIFLASH_SECTOR_OFFSET + 0x1000);
+    SPIFlash_EraseSector(0x1000*SPIFLASH_SECTOR_OFFSET + 0x2000);
+    SPIFlash_EraseSector(0x1000*SPIFLASH_SECTOR_OFFSET + 0x3000);
 
     printf("Pgm 2\n");
-    SPIFlash_WriteBytes(0x2000, 0x1000, (u8*)0x08000000);
+    SPIFlash_WriteBytes(0x1000*SPIFLASH_SECTOR_OFFSET + 0x0000, 0x1000, (u8*)0x08000000);
     printf("Pgm 3\n");
-    SPIFlash_WriteBytes(0x3000, 0x1000, (u8*)0x08001000);
+    SPIFlash_WriteBytes(0x1000*SPIFLASH_SECTOR_OFFSET + 0x1000, 0x1000, (u8*)0x08001000);
     printf("Pgm 4\n");
-    SPIFlash_WriteBytes(0x4000, 0x1000, (u8*)0x08002000);
+    SPIFlash_WriteBytes(0x1000*SPIFLASH_SECTOR_OFFSET + 0x2000, 0x1000, (u8*)0x08002000);
     printf("Pgm 5\n");
-    SPIFlash_WriteBytes(0x5000, 0x1000, (u8*)0x08003000);
+    SPIFlash_WriteBytes(0x1000*SPIFLASH_SECTOR_OFFSET + 0x3000, 0x1000, (u8*)0x08003000);
     printf("Done\n");
 
     LCD_Clear(0x0000);
