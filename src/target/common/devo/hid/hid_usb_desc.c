c59e4afe8e321b247d38a2577f14bb739abc4769
PhracturedBlue <rc2012@pblue.org>
2014-01-02 15:36:08 -0800

    Enable 8 channels in USBHID for Windows.  Fixes #444

--- src/target/common/devo/hid/hid_usb_desc.c
+++ src/target/common/devo/hid/hid_usb_desc.c
@@ -101,7 +101,7 @@ const uint8_t Joystick_ConfigDescriptor[JOYSTICK_SIZ_CONFIG_DESC] =
 
     0x81,          /*bEndpointAddress: Endpoint Address (IN)*/
     0x03,          /*bmAttributes: Interrupt endpoint*/
-    0x0C,          /*wMaxPacketSize: 12 Byte max */
+    0x08,          /*wMaxPacketSize: 8 Byte max */
     0x00,
     0x20,          /*bInterval: Polling Interval (32 ms)*/
     /* 34 */
@@ -110,28 +110,30 @@ const uint8_t Joystick_ConfigDescriptor[JOYSTICK_SIZ_CONFIG_DESC] =
 const uint8_t Joystick_ReportDescriptor[JOYSTICK_SIZ_REPORT_DESC] =
   {
     0x05, 0x01,                    // USAGE_PAGE (Generic Desktop)
-    0x09, 0x05,                    // USAGE (Game Pad)
+    0x15, 0x81,                    // LOGICAL_MINIMUM (0)
+    0x25, 0x7f,                    // LOGICAL_MAXIMUM (255)
+    0x75, 0x08,                    // REPORT_SIZE (8)
+    0x09, 0x04,                    // USAGE (Joystick)
     0xa1, 0x01,                    // COLLECTION (Application)
+    0x09, 0x01,                    //   USAGE (Pointer)
     0xa1, 0x00,                    //   COLLECTION (Physical)
-    0x05, 0x01,                    //     USAGE_PAGE (Generic Desktop)
     0x09, 0x30,                    //     USAGE (X)
     0x09, 0x31,                    //     USAGE (Y)
+    0x95, 0x02,                    //     REPORT_COUNT (2)
+    0x81, 0x82,                    //     INPUT (Data,Var,Abs,Vol)
+    0xc0,                          //   END_COLLECTION
+    0xa1, 0x00,                    //   COLLECTION (Physical)
     0x09, 0x32,                    //     USAGE (Z)
     0x09, 0x33,                    //     USAGE (Rx)
-    0x09, 0x34,                    //     USAGE (Ry)
-    0x09, 0x35,                    //     USAGE (Rz)
-    0x09, 0x36,                    //     USAGE (Slider)
-    0x09, 0x36,                    //     USAGE (Slider)
-    0x09, 0x36,                    //     USAGE (Slider)
-    0x09, 0x36,                    //     USAGE (Slider)
-    0x09, 0x36,                    //     USAGE (Slider)
-    0x09, 0x36,                    //     USAGE (Slider)
-    0x15, 0x81,                    //     LOGICAL_MINIMUM (-127)
-    0x25, 0x7f,                    //     LOGICAL_MAXIMUM (127)
-    0x75, 0x08,                    //     REPORT_SIZE (8)
-    0x95, 0x0c,                    //     REPORT_COUNT (12)
-    0x81, 0x02,                    //     INPUT (Data,Var,Abs)
+    0x95, 0x02,                    //     REPORT_COUNT (2)
+    0x81, 0x82,                    //     INPUT (Data,Var,Abs,Vol)
     0xc0,                          //   END_COLLECTION
+    0x09, 0x34,                    //   USAGE (Ry)
+    0x09, 0x35,                    //   USAGE (Rz)
+    0x09, 0x36,                    //   USAGE (Slider)
+    0x09, 0x37,                    //   USAGE (Dial)
+    0x95, 0x04,                    //   REPORT_COUNT (4)
+    0x81, 0x82,                    //   INPUT (Data,Var,Abs,Vol)
     0xc0                           // END_COLLECTION
   }
   ; /* Joystick_ReportDescriptor */
