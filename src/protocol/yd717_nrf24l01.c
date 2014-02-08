e015c980773c5c29b620323aa7769dd43882c5ea
hexfet <devnull@localhost>
2014-02-07 20:39:02 -0500

    Add SymaX2+ protocol to support Syma quadcopters.  Tested with Syma X4 and X6.  May also support X2, X3, X5C, and X7. Implemented in yd717_nrf24l01.c file since there are only minor differences: - different bind address - additional checksum byte in data packets - trim values at different bytes in data packets
    Also changed the bind process to match Syma X4, which is compatible with YD717.  Binding now transmits all packets
    on the bind address instead of just the last packet.  Should be more robust.

--- src/protocol/yd717_nrf24l01.c
+++ src/protocol/yd717_nrf24l01.c
@@ -69,9 +69,10 @@ enum {
     CHANNEL10
 };
 
-#define PAYLOADSIZE 8
+#define PAYLOADSIZE 8       // receive data pipes set to this size, but unused
+#define MAX_PACKET_SIZE 9   // YD717 packets have 8-byte payload, Syma X4 is 9
 
-static u8 packet[PAYLOADSIZE];
+static u8 packet[MAX_PACKET_SIZE];
 static u8 packet_sent;
 static u16 counter;
 static u32 packet_counter;
@@ -95,11 +96,13 @@ static const char * const yd717_opts[] = {
 enum {
     PROTOOPTS_TELEMETRY = 0,
 };
-#define TELEM_ON 1
 #define TELEM_OFF 0
+#define TELEM_ON 1
 
 // Bit vector from bit position
 #define BV(bit) (1 << bit)
+
+
 
 static u8 packet_ack()
 {
@@ -112,14 +115,131 @@ static u8 packet_ack()
     return status & BV(NRF24L01_07_TX_DS);
 }
 
+
 static void packet_write()
 {
     packet_ack();
 
     NRF24L01_FlushTx();
-    NRF24L01_WritePayload(packet, sizeof(packet));
+
+    if(Model.protocol == PROTOCOL_YD717) {
+        NRF24L01_WritePayload(packet, 8);
+    } else {
+        packet[8] = packet[0];  // checksum
+        for(u8 i=1; i < 8; i++) packet[8] += packet[i];
+        packet[8] = ~packet[8];
+
+        NRF24L01_WritePayload(packet, 9);
+    }
+
     ++packet_counter;
     packet_sent = 1;
+}
+
+
+static u8 convert_channel(u8 num)
+{
+    // Sometimes due to imperfect calibration or mixer settings
+    // throttle can be less than CHAN_MIN_VALUE or larger than
+    // CHAN_MAX_VALUE. As we have no space here, we hard-limit
+    // channels values by min..max range
+    s32 ch = Channels[num];
+    if (ch < CHAN_MIN_VALUE) {
+        ch = CHAN_MIN_VALUE;
+    } else if (ch > CHAN_MAX_VALUE) {
+        ch = CHAN_MAX_VALUE;
+    }
+
+    return (u8) (((ch * 0xFF / CHAN_MAX_VALUE) + 0x100) >> 1);
+}
+
+
+static void read_controls(u8* throttle, u8* rudder, u8* elevator, u8* aileron,
+                          u8* flags, u8* rudder_trim, u8* elevator_trim, u8* aileron_trim)
+{
+    // Protocol is registered AETRF, that is
+    // Aileron is channel 1, Elevator - 2, Throttle - 3, Rudder - 4, Flip control - 5
+
+    // Channel 3
+    *throttle = convert_channel(CHANNEL3);
+
+    // Channel 4
+    *rudder = 0xff - convert_channel(CHANNEL4);
+    *rudder_trim = *rudder >> 1;
+
+    // Channel 2
+    *elevator = convert_channel(CHANNEL2);
+    *elevator_trim = *elevator >> 1;
+
+    // Channel 1
+    *aileron = 0xff - convert_channel(CHANNEL1);
+    *aileron_trim = *aileron >> 1;
+
+    // Channel 5
+    if (Channels[CHANNEL5] <= 0)
+      *flags &= ~FLAG_FLIP;
+    else
+      *flags |= FLAG_FLIP;
+
+    // Print channels every second or so
+    if ((packet_counter & 0xFF) == 1) {
+        printf("Raw channels: %d, %d, %d, %d, %d, %d, %d, %d\n",
+               Channels[0], Channels[1], Channels[2], Channels[3],
+               Channels[4], Channels[5], Channels[6], Channels[7]);
+        printf("Aileron %d, elevator %d, throttle %d, rudder %d, flip enable %d\n",
+               (s16) *aileron, (s16) *elevator, (s16) *throttle, (s16) *rudder,
+               (s16) *flags);
+    }
+}
+
+
+static void send_packet(u8 bind)
+{
+    if (bind) {
+        packet[0]= rx_tx_addr[0]; // send data phase address in first 4 bytes
+        packet[1]= rx_tx_addr[1];
+        packet[2]= rx_tx_addr[2];
+        packet[3]= rx_tx_addr[3];
+        packet[4] = 0x56;
+        packet[5] = 0xAA;
+        packet[6] = 0x32;
+        packet[7] = 0x00;
+    } else {
+        read_controls(&throttle, &rudder, &elevator, &aileron, &flags, &rudder_trim, &elevator_trim, &aileron_trim);
+        packet[0] = throttle;
+        packet[1] = rudder;
+        packet[3] = elevator;
+        packet[4] = aileron;
+        if(Model.protocol == PROTOCOL_YD717) {
+          packet[2] = elevator_trim;
+          packet[5] = aileron_trim;
+          packet[6] = rudder_trim;
+        } else {
+          packet[2] = rudder_trim;
+          packet[5] = elevator_trim;
+          packet[6] = aileron_trim;
+        }
+        packet[7] = flags;
+    }
+
+    packet_write();
+
+//    radio.ce(HIGH);
+//    delayMicroseconds(15);
+    // It saves power to turn off radio after the transmission,
+    // so as long as we have pins to do so, it is wise to turn
+    // it back.
+//    radio.ce(LOW);
+
+    // Check and adjust transmission power. We do this after
+    // transmission to not bother with timeout after power
+    // settings change -  we have plenty of time until next
+    // packet.
+    if (tx_power != Model.tx_power) {
+        //Keep transmit power updated
+        tx_power = Model.tx_power;
+        NRF24L01_SetPower(tx_power);
+    }
 }
 
 static void yd717_init()
@@ -195,43 +315,24 @@ static void yd717_init()
     // delay(50);
 }
 
+
 static void YD717_init1()
 {
-    // receiver doesn't re-enter bind mode if connection lost...check if already bound
-    packet[0] = 0; 
-    packet[1] = 0x80;
-    packet[3] = 0x80;
-    packet[4] = 0x80;
-    packet[2] = 0x40;
-    packet[5] = 0x40;
-    packet[6] = 0x40;
-    packet[7] = 0;
-    packet_write();
-}
-
-static void YD717_init2()
-{
-    packet_ack();   // acknowledge last bind packet.  Must be complete before changing address
-
-    // set address to prearranged value known to receiver (guessing...)
+    // for bind packets set address to prearranged value known to receiver
     u8 bind_rx_tx_addr[] = {0x65, 0x65, 0x65, 0x65, 0x65};
+    if(Model.protocol == PROTOCOL_SymaX)
+        for(u8 i=0; i < 5; i++) bind_rx_tx_addr[i]  = 0x60;
+
     NRF24L01_WriteRegisterMulti(NRF24L01_0A_RX_ADDR_P0, bind_rx_tx_addr, 5);
     NRF24L01_WriteRegisterMulti(NRF24L01_10_TX_ADDR, bind_rx_tx_addr, 5);
 
-    packet[0]= rx_tx_addr[0]; // send address to use in first 4 bytes
-    packet[1]= rx_tx_addr[1];
-    packet[2]= rx_tx_addr[2];
-    packet[3]= rx_tx_addr[3];
-    packet[4]= 0x56; // fixed value
-    packet[5]= 0xAA; // fixed value
-    packet[6]= 0x32; // fixed - same as trim values in bind packets...
-    packet[7]= 0x00; // fixed value
-    packet_write();
+    send_packet(1);
 }
 
-static u8 YD717_init3()
+
+static u8 YD717_init2()
 {
-    u8 status = packet_ack();   // acknowledge packet sent by init3.  Must be complete before changing address
+    u8 status = packet_ack();   // acknowledge packet - must be complete before changing address
 
     // set address
     NRF24L01_WriteRegisterMulti(NRF24L01_0A_RX_ADDR_P0, rx_tx_addr, 5);
@@ -241,103 +342,6 @@ static u8 YD717_init3()
     return status;
 }
 
-static u8 convert_channel(u8 num)
-{
-    // Sometimes due to imperfect calibration or mixer settings
-    // throttle can be less than CHAN_MIN_VALUE or larger than
-    // CHAN_MAX_VALUE. As we have no space here, we hard-limit
-    // channels values by min..max range
-    s32 ch = Channels[num];
-    if (ch < CHAN_MIN_VALUE) {
-        ch = CHAN_MIN_VALUE;
-    } else if (ch > CHAN_MAX_VALUE) {
-        ch = CHAN_MAX_VALUE;
-    }
-
-    return (u8) (((ch * 0xFF / CHAN_MAX_VALUE) + 0x100) >> 1);
-}
-
-static void read_controls(u8* throttle, u8* rudder, u8* elevator, u8* aileron,
-                          u8* flags, u8* rudder_trim, u8* elevator_trim, u8* aileron_trim)
-{
-    // Protocol is registered AETRF, that is
-    // Aileron is channel 1, Elevator - 2, Throttle - 3, Rudder - 4, Flip control - 5
-
-    // Channel 3
-    *throttle = convert_channel(CHANNEL3);
-
-    // Channel 4
-    *rudder = 0xff - convert_channel(CHANNEL4);
-    *rudder_trim = *rudder >> 1;
-
-    // Channel 2
-    *elevator = convert_channel(CHANNEL2);
-    *elevator_trim = *elevator >> 1;
-
-    // Channel 1
-    *aileron = 0xff - convert_channel(CHANNEL1);
-    *aileron_trim = *aileron >> 1;
-
-    // Channel 5
-    if (Channels[CHANNEL5] <= 0)
-      *flags &= ~FLAG_FLIP;
-    else
-      *flags |= FLAG_FLIP;
-
-    // Print channels every second or so
-    if ((packet_counter & 0xFF) == 1) {
-        printf("Raw channels: %d, %d, %d, %d, %d, %d, %d, %d\n",
-               Channels[0], Channels[1], Channels[2], Channels[3],
-               Channels[4], Channels[5], Channels[6], Channels[7]);
-        printf("Aileron %d, elevator %d, throttle %d, rudder %d, flip enable %d\n",
-               (s16) *aileron, (s16) *elevator, (s16) *throttle, (s16) *rudder,
-               (s16) *flags);
-    }
-}
-
-static void send_packet(u8 bind)
-{
-    if (bind) {
-        packet[0] = 0;         // stock tx sends actual channel values during bind
-        packet[1] = 0x80;
-        packet[3] = 0x80;
-        packet[4] = 0x80;
-        packet[2] = 0x32;      // trims have fixed values for bind
-        packet[5] = 0x32;
-        packet[6] = 0x32;
-        packet[7] = 0;
-    } else {
-        // regular packet
-        read_controls(&throttle, &rudder, &elevator, &aileron, &flags, &rudder_trim, &elevator_trim, &aileron_trim);
-        packet[0] = throttle;
-        packet[1] = rudder;
-        packet[3] = elevator;
-        packet[4] = aileron;
-        packet[2] = elevator_trim;
-        packet[5] = aileron_trim;
-        packet[6] = rudder_trim;
-        packet[7] = flags;
-    }
-
-    packet_write();
-
-//    radio.ce(HIGH);
-//    delayMicroseconds(15);
-    // It saves power to turn off radio after the transmission,
-    // so as long as we have pins to do so, it is wise to turn
-    // it back.
-//    radio.ce(LOW);
-
-    // Check and adjust transmission power. We do this after
-    // transmission to not bother with timeout after power
-    // settings change -  we have plenty of time until next
-    // packet.
-    if (tx_power != Model.tx_power) {
-        //Keep transmit power updated
-        tx_power = Model.tx_power;
-        NRF24L01_SetPower(tx_power);
-    }
-}
 
 static void update_telemetry() {
   static u8 frameloss = 0;
@@ -355,26 +359,27 @@ static u16 yd717_callback()
 {
     switch (phase) {
     case YD717_INIT1:
-        YD717_init1();        // check if receiver already in data mode
+        send_packet(0);      // receiver doesn't re-enter bind mode if connection lost...check if already bound
         phase = YD717_BIND3;
         MUSIC_Play(MUSIC_TELEMALARM1);
         break;
     case YD717_BIND2:
         if (--counter == 0) {
-            YD717_init2();    // send rx/tx address to use on fixed bind address
+            YD717_init2();  // change to data phase rx/tx address
+            send_packet(0);
             phase = YD717_BIND3;
-            return 150;
         } else {
             send_packet(1);
         }
         break;
     case YD717_BIND3:
-        if (YD717_init3()) {
+        if (YD717_init2()) {
           flags = 0;
           phase = YD717_DATA;
           PROTOCOL_SetBindState(0);
           MUSIC_Play(MUSIC_DONE_BINDING);
         } else {
+          YD717_init1();
           counter = BIND_COUNT;
           phase = YD717_BIND2;
         }
