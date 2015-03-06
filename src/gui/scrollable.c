eb4f9e5467037a3ad9f4173ff17100ad8f072d86
Indigo1 <indigo@sanitytechnology.com>
2015-03-06 19:58:56 +1000

    Fix issue #606 - Channel Monitor crashes when using small fonts on Devo10

--- src/gui/scrollable.c
+++ src/gui/scrollable.c
@@ -50,7 +50,7 @@ guiObject_t *GUI_CreateScrollable(guiScrollable_t *scrollable, u16 x, u16 y, u16
 
     box->x = x;
     box->y = y;
-    box->width = width - ARROW_WIDTH;
+    box->width = width;
     box->height = height;
 
     obj->Type = Scrollable;
@@ -171,6 +171,8 @@ static int adjust_row(guiScrollable_t *scrollable, int target_row, int offset)
     }
     if (offset <= 0 && first_row - height == target)
         first_row = target;
+    else if (height < 0 && first_row < target)
+        first_row++;
 
     //printf("First row %d -> %d \tcount %d\n", target_row, first_row, last_row - first_row);
     return first_row;
@@ -196,7 +198,7 @@ static int create_scrollable_objs(guiScrollable_t *scrollable, int row, int offs
     guiObject_t *head = objHEAD;
     objHEAD = NULL;
     for(int y = scrollable->header.box.y, bottom = y + scrollable->header.box.height;
-        y < bottom && row < scrollable->item_count;
+        row < scrollable->item_count && rel_row < scrollable->max_visible_rows;
         row++, rel_row++)
     {
         selectable = scrollable->row_cb(row, rel_row, y, scrollable->cb_data);
