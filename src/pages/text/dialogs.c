a8302c3abaddb01f876f1b4591d860685d877a17
PhracturedBlue <rc2012@pblue.org>
2015-05-17 19:31:15 -0700

    Merge dialogs page between text and 128x64 interfaces

--- src/pages/text/dialogs.c
+++ src/pages/text/dialogs.c
@@ -19,157 +19,22 @@
 #include "config/model.h"
 #include "config/ini.h"
 
-#define MAX_CONCURRENT_SAFETY_MSGS 1
+#define OVERRIDE_PLACEMENT
 
-#include "../common/_dialogs.c"
-
-static struct dialog_obj * const gui = &gui_objs.dialog;
-
-static u32 dialogcrc;
-
-void PAGE_ShowSafetyDialog()
-{
-    if (dialog) {
-        u64 unsafe = safety_check();
-        if (! unsafe) {
-            DialogClose(dialog, 0);
-            safety_confirmed();
-        } else {
-            safety_string_cb(NULL, NULL);
-            u32 crc = Crc(tempstring, strlen(tempstring));
-            if (crc != dialogcrc) {
-                GUI_Redraw(dialog);
-                dialogcrc = crc;
-            }
-        }
-    } else {
-        tempstring[0] = 0;
-        dialogcrc = 0;
-        dialog = GUI_CreateDialog(&gui->dialog, 2, 5, LCD_WIDTH - 4, LCD_HEIGHT - 10, NULL, safety_string_cb, safety_ok_cb, dtOk, NULL);
-    }
-}
-
-/******************/
-/* Binding Dialog */
-/******************/
-static const char *binding_string_cb(guiObject_t *obj, void *data)
-{
-    (void)data;
-    u32 crc = Crc(tempstring, strlen(tempstring));
-    if (obj && crc == dialogcrc)
-        return tempstring;
-    u32 bind_time = PROTOCOL_Binding();
-    char tmp[10];
-    snprintf(tmp, 10, "%s", Model.name);
-    snprintf(tempstring, sizeof(tempstring), _tr("Binding %s...\nPress ENT to stop"), tmp);
-    if (bind_time != 0xFFFFFFFF ) {
-        int len = strlen(tempstring);
-        snprintf(tempstring + len, sizeof(tempstring) - len, _tr("\n%d seconds left"), (int)bind_time / 1000);
-    }
-    return tempstring;
-}
-static void binding_ok_cb(u8 state, void * data)
-{
-    (void)state;
-    (void)data;
-    PROTOCOL_SetBindState(0); // interrupt the binding
-    PAGE_CloseBindingDialog();
-}
-
-void PAGE_CloseBindingDialog()
-{
-    if (dialog) {
-        DialogClose(dialog, 0);
-    }
-}
-
-void PAGE_ShowBindingDialog(u8 update)
-{
-    if (update && ! dialog)
-        return;
-    binding_string_cb(NULL, NULL);
-    u32 crc = Crc(tempstring, strlen(tempstring));
-    if (dialog && crc != dialogcrc) {
-        GUI_Redraw(dialog);
-    } else if(! dialog) {
-        dialog = GUI_CreateDialog(&gui->dialog, 0, 3, LCD_WIDTH, LCD_HEIGHT-5, NULL, NULL, binding_ok_cb, dtOk, tempstring);
-    }
-    dialogcrc = crc;
-}
-
-void PAGE_ShowWarning(const char *title, const char *str)
-{
-    (void)title;
-    if (dialog)
-        return;
-    if (str != tempstring)
-        tempstring_cpy(str);
-    dialog = GUI_CreateDialog(&gui->dialog, 0, 3, LCD_WIDTH, LCD_HEIGHT-5, NULL, NULL, lowbatt_ok_cb, dtOk, tempstring);
-}
-
-
-void PAGE_ShowLowBattDialog()
-{
-    PAGE_ShowWarning(NULL, _tr("Battery too low,\ncan't save!"));
-}
-
-const char *invalidstdmixer_string_cb(guiObject_t *obj, void *data)
-{
-    (void)obj;
-    (void)data;
-    return _tr("Model needs reset\nfor standard mixer");
-}
-
-void PAGE_ShowInvalidStandardMixerDialog(void *guiObj)
-{
-    if (dialog)
-        return;
-
-    tempstring[sizeof(tempstring) - 1] = 0;
-    dialog = GUI_CreateDialog(&gui->dialog, 0, 3, LCD_WIDTH, LCD_HEIGHT-5, NULL,
-            invalidstdmixer_string_cb,
-            invalid_stdmixer_cb, dtOkCancel, guiObj);
-}
-
-void PAGE_ShowInvalidModule()
-{
-    PAGE_ShowWarning(NULL, _tr("Bad/missing\nprotocol modules!"));
-}
-
-/********************************/
-/* Reset Permanent Timer Dialog */
-/********************************/
-
-void PAGE_ShowResetPermTimerDialog(void *guiObject, void *data)
-{
-    (void)guiObject;
-    if (dialog)
-        return;
-    dialog = GUI_CreateDialog(&gui->dialog, 1 , 3,  LCD_WIDTH-2, LCD_HEIGHT-5 , NULL , reset_timer_string_cb, reset_permtimer_cb, dtOkCancel, data);
-}
-/********************************/
-/* Show Missing Transceiver Modules */
-/********************************/
-void PAGE_ShowModuleDialog(const char **missing)
-{
-    if (dialog)
-        return;
-    dialogcrc = 0;
-    int count = 0;
-    sprintf(tempstring, "%s", _tr("Missing Modules:\n"));
-    if(missing[MULTIMOD]) {
-        sprintf(tempstring+strlen(tempstring), "%s", missing[MULTIMOD]);
-    } else {
-        for(int i = 0; i < MULTIMOD; i++) {
-           if(missing[i]) {
-               if(! count) {
-                   sprintf(tempstring+strlen(tempstring), " %s", missing[i]);
-               } else if(count == 1) {
-                   sprintf(tempstring+strlen(tempstring), " ...");
-               }
-               count++;
-           }
-        }
-    } 
-    PAGE_ShowWarning(NULL, tempstring);
-}
+enum {
+    DIALOG1_X      = 2 * ITEM_SPACE,
+    DIALOG1_Y      = 2 * LINE_HEIGHT,
+    DIALOG1_WIDTH  = LCD_WIDTH - 4 * ITEM_SPACE,
+    DIALOG1_HEIGHT = LCD_HEIGHT - 2 * LINE_HEIGHT,
+    //
+    DIALOG2_X      = 2 * ITEM_SPACE,
+    DIALOG2_Y      = 2 * LINE_HEIGHT,
+    DIALOG2_WIDTH  = LCD_WIDTH - 4 * ITEM_SPACE,
+    DIALOG2_HEIGHT = LCD_HEIGHT - 2 * LINE_HEIGHT,
+    //
+    DIALOG3_X      = 3 * ITEM_SPACE,
+    DIALOG3_Y      = 3 * LINE_HEIGHT,
+    DIALOG3_WIDTH  = LCD_WIDTH - 6 * ITEM_SPACE,
+    DIALOG3_HEIGHT = LCD_HEIGHT - 3 * LINE_HEIGHT,
+};
+#include "../128x64x1/dialogs.c"
