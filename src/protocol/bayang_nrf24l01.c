88fc73138b49e49d6b34f6288beef473c2d21432
goebish <goebish@gmail.com>
2015-12-22 16:01:30 +0100

    Add video and snapshot channels for H8C mini

--- src/protocol/bayang_nrf24l01.c
+++ src/protocol/bayang_nrf24l01.c
@@ -142,7 +142,9 @@ static void send_packet(u8 bind)
         packet[1] = 0xfa;   // normal mode is 0xf7, expert 0xfa
         packet[2] = GET_FLAG(CHANNEL_FLIP, 0x08)
                   | GET_FLAG(CHANNEL_HEADLESS, 0x02)
-                  | GET_FLAG(CHANNEL_RTH, 0x01);
+                  | GET_FLAG(CHANNEL_RTH, 0x01)
+                  | GET_FLAG(CHANNEL_VIDEO, 0x10)
+                  | GET_FLAG(CHANNEL_PICTURE, 0x20);
         packet[3] = GET_FLAG(CHANNEL_INVERTED, 0x80);
         chanval.value = scale_channel(CHANNEL1, 0x3ff, 0);   // aileron
         packet[4] = chanval.bytes.msb + DYNTRIM(chanval.value);
