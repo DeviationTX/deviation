72a3c03fe6c4785a76a6421e1003bbf31e998ac7
victzh <victzh@gmail.com>
2016-03-17 10:47:24 -0400

    S-FHSS initial implementation

--- src/protocol/iface_cc2500.h
+++ src/protocol/iface_cc2500.h
@@ -129,6 +129,8 @@ void CC2500_WriteReg(u8 addr, u8 data);
 u8 CC2500_ReadReg(u8 addr);
 int CC2500_Reset();
 void CC2500_Strobe(u8 cmd);
+void CC2500_WriteRegisterMulti(u8 address, const u8 data[], u8 length);
+void CC2500_ReadRegisterMulti(u8 address, u8 data[], u8 length);
 void CC2500_WriteData(u8 *packet, u8 length);
 void CC2500_ReadData(u8 *dpbuffer, int len);
 void CC2500_SetTxRxMode(enum TXRX_State);
