ec8ef62090eb09c8736b50e4bfdf153016adab9f
Alexander Tuxen <github@arles-electrique.de>
2016-07-03 09:34:21 +0200

    started fixing universaltx build

--- src/universaltx/include/config/tx.h
+++ src/universaltx/include/config/tx.h
@@ -1,8 +1,16 @@
 #ifndef _TX_H_
 #define _TX_H_
 
+enum {
+    CYRF6936_DEVO       = 0x00,
+    CYRF6936_AWA24S     = 0x01,
+    CYRF6936_BUYCHINA   = 0x02,
+    CC2500_REVERSE_GD02 = 0x01,
+};
+
 /* note that MODULE_ENABLE is defined in protospi.h */
 struct Transmitter {
+    u8 module_config[TX_MODULE_LAST];
     u32 txid;
     //u8 module_poweramp;
 };
