14e7d244cbd800357487a93d9a4480d1d9d70265
Martin Budden <mjbudden@gmail.com>
2016-09-08 14:56:36 +0100

    Changed to use single payload size. Code tidy.

--- src/protocol/inav_nrf24l01.c
+++ src/protocol/inav_nrf24l01.c
@@ -61,10 +61,9 @@
 #define USE_AUTO_ACKKNOWLEDGEMENT
 
 // Telemetry not yet implemented, requires AUTO_ACK
-//#define INAV_TELEMETRY
+#define INAV_TELEMETRY
 //#define INAV_TELEMETRY_DEBUG
 
-//#define INAV_CHANNEL_OPTS
 
 #ifdef MODULAR
   //Allows the linker to properly relocate
@@ -184,9 +183,6 @@ enum {
 };
 
 static const char * const inav_opts[] = {
-#ifdef INAV_CHANNEL_OPTS
-  _tr_noop("Channels"),  _tr_noop("6"), _tr_noop("12"), NULL,
-#endif
 #ifdef INAV_TELEMETRY
   _tr_noop("Telemetry"),  _tr_noop("On"), _tr_noop("Off"), NULL,
 #endif
@@ -196,9 +192,6 @@ static const char * const inav_opts[] = {
 };
 
 enum {
-#ifdef INAV_CHANNEL_OPTS
-    PROTOOPTS_CHANNELS = 0,
-#endif
 #ifdef INAV_TELEMETRY
     PROTOOPTS_TELEMETRY,
 #endif
@@ -240,7 +233,7 @@ enum {
 
 #define INAV_PROTOCOL_PAYLOAD_SIZE_MIN      8
 #define INAV_PROTOCOL_PAYLOAD_SIZE_DEFAULT  16
-#define INAV_PROTOCOL_PAYLOAD_SIZE_MAX      18
+#define INAV_PROTOCOL_PAYLOAD_SIZE_MAX      16
 
 static u32 packet_counter;
 static u8 packet[INAV_PROTOCOL_PAYLOAD_SIZE_MAX];
@@ -258,6 +251,9 @@ static u8 tx_power;
 static const u8 rx_tx_addr_bind[RX_TX_ADDR_LEN] = {0x4b,0x5c,0x6d,0x7e,0x8f};
 static u8 rx_tx_addr[RX_TX_ADDR_LEN];
 #define RX_TX_ADDR_4 0xD2 // rxTxAddr[4] always set to this value
+
+#define BIND_ACK_PAYLOAD0 0x83
+#define BIND_ACK_PAYLOAD1 0xa5
 
 #ifdef USE_AUTO_ACKKNOWLEDGEMENT
 static u8 ackPayloadSize;
@@ -328,13 +324,6 @@ static void build_data_packet(void)
     // pack the AETR low bits
     packet[6] = (aileron & 0x03) | ((elevator & 0x03) << 2) | ((throttle & 0x03) << 4) | ((rudder & 0x03) << 6);
 
-    if (payload_size == INAV_PROTOCOL_PAYLOAD_SIZE_MIN) {
-        // small payload variant of protocol, supports 6 channels
-        packet[7] = convert_channel8(CHANNEL5);
-        packet[1] = convert_channel8(CHANNEL6);
-        return;
-    }
-
     u8 rate = RATE_LOW;
     if (Channels[CHANNEL_RATE] > 0) {
         rate = RATE_HIGH;
@@ -351,8 +340,8 @@ static void build_data_packet(void)
     packet[8] = flags; // flags, deviation channels 6-10 are mapped to AUX2 t0 AUX6
 
     // map deviation channels 9-12 to RC channels AUX7-AUX10, use 10 bit resolution
-    // deviation channels 9 (headless) and 10 (RTH) are mapped for a second time
-    // duplicate mapping makes maximum use of Deviation's 12 channels
+    // deviation CHANNEL9 (headless) and CHANNEL10 (RTH) are mapped for a second time
+    // duplicate mapping makes maximum use of deviation's 12 channels
     const u16 channel9 = convert_channel(CHANNEL9);
     const u16 channel10 = convert_channel(CHANNEL10);
     const u16 channel11 = convert_channel(CHANNEL11);
@@ -367,12 +356,6 @@ static void build_data_packet(void)
     // deviation channels 7 (picture) and 8 (video) are mapped for a second time
     packet[14] = convert_channel8(CHANNEL7);
     packet[15] = convert_channel8(CHANNEL8);
-    if (payload_size == INAV_PROTOCOL_PAYLOAD_SIZE_MAX) {
-        // map deviation channels 5 and 6 to RC channels AUX14 and AUX12, use 8 bit resolution
-        // deviation channels 5 (rate) and 6 (flip) are mapped for a second time
-        packet[16] = convert_channel8(CHANNEL5);
-        packet[17] = convert_channel8(CHANNEL6);
-    }
 }
 
 
@@ -380,17 +363,16 @@ static u8 packet_ack(void)
 {
 #ifdef USE_AUTO_ACKKNOWLEDGEMENT
     const u8 status = NRF24L01_ReadReg(NRF24L01_07_STATUS);
-    if ((status & BV(NRF24L01_07_TX_DS))) { // NRF24L01_07_TX_DS asserted when ack payload received
+    if (status & BV(NRF24L01_07_TX_DS)) { // NRF24L01_07_TX_DS asserted when ack payload received
         // ack payload recieved
         ackPayloadSize = NRF24L01_GetDynamicPayloadSize();
         if (ackPayloadSize > NRF24L01_MAX_PAYLOAD_SIZE) {
-            // packet contains errors and must be discarded
             ackPayloadSize = 0;
-        } else {
-            NRF24L01_ReadPayload(ackPayload, ackPayloadSize);
+            NRF24L01_FlushRx();
+            return PKT_PENDING;
         }
+        NRF24L01_ReadPayload(ackPayload, ackPayloadSize);
         NRF24L01_WriteReg(NRF24L01_07_STATUS, BV(NRF24L01_07_TX_DS)); // clear TX_DS interrupt
-        NRF24L01_FlushRx();
         return PKT_ACKED;
     }
     if (status & BV(NRF24L01_07_MAX_RT)) {
@@ -408,10 +390,10 @@ static u8 packet_ack(void)
 
 void transmit_packet(void)
 {
-    // clear packet status bits and TX FIFO
-    NRF24L01_WriteReg(NRF24L01_07_STATUS, BV(NRF24L01_07_RX_DR) | BV(NRF24L01_07_TX_DS) | BV(NRF24L01_07_MAX_RT));
+    // clear packet MAX_RT status bit so that transmission is not blocked
+    NRF24L01_WriteReg(NRF24L01_07_STATUS, BV(NRF24L01_07_MAX_RT));
     // set NRF24L01_00_MASK_TX_DS and clear NRF24L01_00_PRIM_RX to initiate transmit
-    NRF24L01_WriteReg(NRF24L01_00_CONFIG, BV(NRF24L01_00_PWR_UP) | BV(NRF24L01_00_CRCO) | BV(NRF24L01_00_EN_CRC) | BV(NRF24L01_00_MASK_TX_DS));
+//    NRF24L01_WriteReg(NRF24L01_00_CONFIG, BV(NRF24L01_00_PWR_UP) | BV(NRF24L01_00_CRCO) | BV(NRF24L01_00_EN_CRC) | BV(NRF24L01_00_MASK_TX_DS) | BV(NRF24L01_00_MASK_MAX_RT));
     NRF24L01_WritePayload(packet, payload_size);// asynchronous call
 }
 
@@ -473,16 +455,20 @@ static void init_nrf24l01(void)
     // sets PWR_UP, EN_CRC, CRCO - 2 byte CRC
     NRF24L01_WriteReg(NRF24L01_00_CONFIG, BV(NRF24L01_00_PWR_UP) | BV(NRF24L01_00_EN_CRC) | BV(NRF24L01_00_CRCO));
 
-    NRF24L01_SetTxRxMode(TX_EN); // enter transmit mode
-
 #ifdef USE_AUTO_ACKKNOWLEDGEMENT
+    // Note that for some cheap NFR24L01 clones the NO_ACK bit in the packet control field is inverted,
+    // see https://ncrmnt.org/2015/03/13/how-do-i-cost-optimize-nrf24l01/
+    // (see section 7.3.3, p28 of nRF24L01+ Product Specification for descripton of packet control field).
+    // This means AUTO_ACK will no work between them and genuine NRF24L01 modules, although AUTO_ACK
+    // between these clones should work.
     NRF24L01_WriteReg(NRF24L01_01_EN_AA, BV(NRF24L01_01_ENAA_P0)); // auto acknowledgment on P0
     NRF24L01_WriteReg(NRF24L01_02_EN_RXADDR, BV(NRF24L01_02_ERX_P0)); // Enable RX on P0 for Auto Ack
 #else
     NRF24L01_WriteReg(NRF24L01_01_EN_AA, 0x00);
 #endif
     NRF24L01_WriteReg(NRF24L01_03_SETUP_AW, NRF24L01_03_SETUP_AW_5BYTES); // 5-byte RX/TX address
-    NRF24L01_WriteReg(NRF24L01_04_SETUP_RETR, NRF24L01_04_ARD_2500us | NRF24L01_04_ARC_1); // one retry after 2500us
+    // ARD of 1500us is sufficient for 32 byte ACK payload in  250kbps mode (section 7.4.2, p33 of nRF24L01+ Product Specification)
+    NRF24L01_WriteReg(NRF24L01_04_SETUP_RETR, NRF24L01_04_ARD_1500us | NRF24L01_04_ARC_1); // 1 retry after 1500us
     NRF24L01_WriteReg(NRF24L01_05_RF_CH, RF_CHANNEL_BIND);
     // bitrate and power are set using regigister NRF24L01_06_RF_SETUP
     NRF24L01_SetBitrate(NRF24L01_BR_250K);
@@ -494,16 +480,15 @@ static void init_nrf24l01(void)
     NRF24L01_WriteRegisterMulti(NRF24L01_0A_RX_ADDR_P0, rx_tx_addr_bind, RX_TX_ADDR_LEN); // RX_ADDR_P0 must equal TX_ADDR for auto ACK
     // RX_ADDR for pipes P1-P5 (registers 0B to 0F) aret left at default values
     NRF24L01_WriteRegisterMulti(NRF24L01_10_TX_ADDR, rx_tx_addr_bind, RX_TX_ADDR_LEN);
-    NRF24L01_WriteReg(NRF24L01_11_RX_PW_P0, payload_size);
-    // Payload Widths for P1-P5 (registers 12 to 16) aret left at default values
+    // Payload Widths for P0-P5 (registers 11 to 16) aret left at default values
     // NRF24L01_17_FIFO_STATUS is read only register for all non-reserved bits
     // No registers with values 18 to 1B
 #ifdef USE_AUTO_ACKKNOWLEDGEMENT
     NRF24L01_ReadReg(NRF24L01_1D_FEATURE);
-    NRF24L01_Activate(0x73); // Activate feature register, needed for NRF24L01 (harmless for NRF24L01P)
+    NRF24L01_Activate(0x73); // Activate feature register, needed for NRF24L01 (harmless for NRF24L01+)
     NRF24L01_ReadReg(NRF24L01_1D_FEATURE);
-    NRF24L01_WriteReg(NRF24L01_1D_FEATURE, BV(NRF24L01_1D_EN_ACK_PAY) | BV(NRF24L01_1D_EN_DPL));
     NRF24L01_WriteReg(NRF24L01_1C_DYNPD, BV(NRF24L01_1C_DYNPD_P0)); // dynamic payload length on pipes P0
+    NRF24L01_WriteReg(NRF24L01_1D_FEATURE, BV(NRF24L01_1D_EN_ACK_PAY) | BV(NRF24L01_1D_EN_DPL));
 #endif
 
     // Check for Beken BK2421/BK2423 chip
@@ -526,12 +511,6 @@ static void init_nrf24l01(void)
         NRF24L01_WriteRegisterMulti(0x03, (u8 *) "\x99\x00\x39\x21", 4);
         NRF24L01_WriteRegisterMulti(0x04, (u8 *) "\xF9\x96\x82\x1B", 4);
         NRF24L01_WriteRegisterMulti(0x05, (u8 *) "\x24\x06\x7F\xA6", 4);
-        NRF24L01_WriteRegisterMulti(0x06, (u8 *) "\x00\x00\x00\x00", 4);
-        NRF24L01_WriteRegisterMulti(0x07, (u8 *) "\x00\x00\x00\x00", 4);
-        NRF24L01_WriteRegisterMulti(0x08, (u8 *) "\x00\x00\x00\x00", 4);
-        NRF24L01_WriteRegisterMulti(0x09, (u8 *) "\x00\x00\x00\x00", 4);
-        NRF24L01_WriteRegisterMulti(0x0A, (u8 *) "\x00\x00\x00\x00", 4);
-        NRF24L01_WriteRegisterMulti(0x0B, (u8 *) "\x00\x00\x00\x00", 4);
         NRF24L01_WriteRegisterMulti(0x0C, (u8 *) "\x00\x12\x73\x00", 4);
         NRF24L01_WriteRegisterMulti(0x0D, (u8 *) "\x46\xB4\x80\x00", 4);
         NRF24L01_WriteRegisterMulti(0x0E, (u8 *) "\x41\x10\x04\x82\x20\x08\x08\xF2\x7D\xEF\xFF", 11);
@@ -541,6 +520,8 @@ static void init_nrf24l01(void)
         dbgprintf("nRF24L01 detected\n");
     }
     NRF24L01_Activate(0x53); // switch bank back
+
+    NRF24L01_SetTxRxMode(TX_EN); // enter transmit mode, sets up NRF24L01_00_CONFIG register
 }
 
 // Generate address to use from TX id and manufacturer id (STM32 unique id)
@@ -571,7 +552,7 @@ static void initialize_rx_tx_addr(void)
         rand32_r(&lfsr, i);
     }
     rx_tx_addr[3] &= 0x7f; // clear top bit, rx_tx_addr can be stored easily as a signed value
-    rx_tx_addr[4] = RX_TX_ADDR_4; // set to constant to, so variable part of rx_tx_addr can be stored in 32 value
+    rx_tx_addr[4] = RX_TX_ADDR_4; // set to constant, so variable part of rx_tx_addr can be stored in 32-bit value
 }
 
 // The hopping channels are determined by the value of rx_tx_addr[0]
@@ -589,38 +570,27 @@ static void set_hopping_channels(void)
 #ifdef INAV_TELEMETRY_DEBUG
 void debug_telemetry(void)
 {
-    ackPayload[1] = 'G';
-    const s32 latitude = 11000000 + (22 * 1000000 / 60) + (33 * 1000000 / 3600);
-    memcpy(&ackPayload[2], &latitude, sizeof(s32));
-
-    const s32 longitude = 15000000 + (36 * 1000000 / 60) + (47 * 1000000 / 3600);
-    memcpy(&ackPayload[6], &longitude, sizeof(s32));
-
-    ackPayload[10] = 37; // speed
-
-    const u32 altitude = 2345L;
-    memcpy(&ackPayload[11], &altitude, sizeof(u32));
-
-    ackPayload[15] = 12 << 2; // satelite count
+    Telemetry.value[TELEM_DSM_FLOG_FADESA] = ackPayloadSize;
+    Telemetry.value[TELEM_DSM_FLOG_FADESB] = ackPayload[0];
+    Telemetry.value[TELEM_DSM_FLOG_FADESL] = ackPayload[1];
+    Telemetry.value[TELEM_DSM_FLOG_FADESR] = ackPayload[2];
+    Telemetry.value[TELEM_DSM_FLOG_HOLDS] = ackPayload[3];
+    TELEMETRY_SetUpdated(TELEM_DSM_FLOG_FADESA);
+    TELEMETRY_SetUpdated(TELEM_DSM_FLOG_FADESB);
+    TELEMETRY_SetUpdated(TELEM_DSM_FLOG_FADESL);
+    TELEMETRY_SetUpdated(TELEM_DSM_FLOG_FADESR);
+    TELEMETRY_SetUpdated(TELEM_DSM_FLOG_HOLDS);
 }
 #endif
 
 static void update_telemetry(void)
 {
 #ifdef INAV_TELEMETRY
-    static u8 frameloss = 0;
-
     const u8 observeTx = NRF24L01_ReadReg(NRF24L01_08_OBSERVE_TX);
     const u8 lostPacketCount = (observeTx & NRF24L01_08_PLOS_CNT_MASK) >> 4;
 //    const u8 autoRetryCount = observeTx & NRF24L01_08_ARC_CNT_MASK;
-    frameloss += lostPacketCount;
     NRF24L01_WriteReg(NRF24L01_05_RF_CH, rf_channels[rf_channel_index]); // reset packet loss counter
-#ifdef INAV_TELEMETRY_DEBUG
-    debug_telemetry();
-    Telemetry.value[TELEM_DSM_FLOG_FRAMELOSS] = ackPayload[0];
-#else
-    Telemetry.value[TELEM_DSM_FLOG_FRAMELOSS] = frameloss;
-#endif
+    Telemetry.value[TELEM_DSM_FLOG_FRAMELOSS] = lostPacketCount;
     TELEMETRY_SetUpdated(TELEM_DSM_FLOG_FRAMELOSS);
 
     // ackPayload contains telemetry in LTM format.
@@ -634,6 +604,7 @@ static void update_telemetry(void)
         // Deviation longitude: +/-180degrees = +/- 180*60*60*1000; W if value<0, E if value>=0; -180degrees = 180degrees
         // Deviation latitude:   +/-90degrees =  +/- 90*60*60*1000; S if value<0, N if value>=0
         // Deviation 1 degree = 3,600,000
+        // Scale factor = 36/10 = 18/5
         s32 latitude;
         memcpy(&latitude, &ackPayload[2], sizeof(s32));
         Telemetry.gps.latitude = latitude * 18 / 5;
@@ -658,15 +629,26 @@ static void update_telemetry(void)
         break;
     case 'A': // Attitude frame
         {
-        //const u16 heading = ackPayload[6] | (ackPayload[7] << 8);
-        const u16 heading = 4567;
+        const u16 heading = ackPayload[6] | (ackPayload[7] << 8);
         Telemetry.gps.heading = heading;
         TELEMETRY_SetUpdated(TELEM_GPS_HEADING);
+
+        // repurpose the FADESL, FADESR and HOLDS fields to display roll, pitch and yaw
+        const s16 pitch = ackPayload[2] | (ackPayload[3] << 8);
+        Telemetry.value[TELEM_DSM_FLOG_FADESL] = pitch; // LTM:degrees [-180,180]
+        TELEMETRY_SetUpdated(TELEM_DSM_FLOG_FADESL);
+
+        const s16 roll = ackPayload[4] | (ackPayload[5] << 8);
+        Telemetry.value[TELEM_DSM_FLOG_FADESR] = roll; // LTM:degrees [-180,180]
+        TELEMETRY_SetUpdated(TELEM_DSM_FLOG_FADESR);
+
+        Telemetry.value[TELEM_DSM_FLOG_HOLDS] = heading;
+        TELEMETRY_SetUpdated(TELEM_DSM_FLOG_HOLDS);
         }
         break;
     case 'S': // Status frame
         {
-            const u16 vBat = ackPayload[2] | (ackPayload[3] << 8);
+        const u16 vBat = ackPayload[2] | (ackPayload[3] << 8);
         Telemetry.value[TELEM_DSM_FLOG_VOLT2] = (s32)(vBat); // LTM:mV; DEV:V/10
         TELEMETRY_SetUpdated(TELEM_DSM_FLOG_VOLT2);
 
@@ -681,6 +663,9 @@ static void update_telemetry(void)
     default:
         break;
     }
+#ifdef INAV_TELEMETRY_DEBUG
+    debug_telemetry();
+#endif
 #endif
 }
 
@@ -696,6 +681,7 @@ void load_rx_tx_addr(void)
     rx_tx_addr[1] = (Model.proto_opts[PROTOOPTS_RX_TX_ADDR1] >> 8) & 0xff;
     rx_tx_addr[2] = (Model.proto_opts[PROTOOPTS_RX_TX_ADDR2]) & 0xff;
     rx_tx_addr[3] = (Model.proto_opts[PROTOOPTS_RX_TX_ADDR2] >> 8) & 0xff;
+    rx_tx_addr[4] = RX_TX_ADDR_4; // set to constant, so variable part of rx_tx_addr can be stored in 32-bit value
 }
 
 static void inav_init(init_bind_t bind)
@@ -703,14 +689,7 @@ static void inav_init(init_bind_t bind)
     // check options before proceeding with initialiasation
     payload_size = INAV_PROTOCOL_PAYLOAD_SIZE_DEFAULT;
     rc_channel_count = RC_CHANNEL_COUNT_DEFAULT;
-    //!!TODO add option to set the number of hopping channels
     rf_channel_count = RF_CHANNEL_COUNT_DEFAULT;
-#ifdef INAV_CHANNEL_OPTS
-    if (Model.proto_opts[PROTOOPTS_CHANNELS] == OPTS_6_CHANNELS) {
-        payload_size = INAV_PROTOCOL_PAYLOAD_SIZE_MIN;
-        rc_channel_count = RC_CHANNEL_COUNT_MIN;
-    }
-#endif
     if (bind) {
         initialize_rx_tx_addr();
         save_rx_tx_addr();
@@ -740,13 +719,15 @@ static u16 inav_callback()
             PROTOCOL_SetBindState(0);
             MUSIC_Play(MUSIC_DONE_BINDING);
         } else {
-            send_packet(BIND_PACKET);
             if (packet_ack() == PKT_ACKED) {
-                // bind packet acked, so set bind_counter to zero to enter data phase on next callback
-                bind_counter = 0;
-            } else {
-                --bind_counter;
+                if (ackPayload[0] == BIND_ACK_PAYLOAD0 && ackPayload[1] == BIND_ACK_PAYLOAD1) {
+                    // bind packet acked, so set bind_counter to zero to enter data phase on next callback
+                    bind_counter = 0;
+                    return PACKET_PERIOD;
+                }
             }
+            send_packet(BIND_PACKET);
+            --bind_counter;
         }
         break;
 
@@ -767,6 +748,7 @@ static void initialize(init_bind_t bind)
 {
     CLOCK_StopTimer();
     tx_power = Model.tx_power;
+    ackPayloadSize = 0;
     memset(ackPayload, 0, NRF24L01_MAX_PAYLOAD_SIZE);
     inav_init(bind);
     init_nrf24l01();
@@ -794,8 +776,8 @@ const void *INAV_Cmds(enum ProtoCmds cmd)
         case PROTOCMD_RESET:
             CLOCK_StopTimer();
             return (void *)(NRF24L01_Reset() ? 1L : -1L);
+        //case PROTOCMD_CHECK_AUTOBIND: return 0; 
         case PROTOCMD_CHECK_AUTOBIND: return (void *)1L; // always Autobind
-        //case PROTOCMD_CHECK_AUTOBIND: return 0;
         case PROTOCMD_BIND:  initialize(INIT_BIND); return 0;
         case PROTOCMD_NUMCHAN: return (void *)((long)rc_channel_count);
         case PROTOCMD_DEFAULT_NUMCHAN: return (void *)((long)RC_CHANNEL_COUNT_DEFAULT);
