8fe38dd62fca87d13338c7ad8683f52fbd187473
vladislavy <vladislavy@rambler.ru>
2016-10-13 13:52:27 +0300

    RF Scanner improvement

--- src/pages/320x240x16/scanner_page.h
+++ src/pages/320x240x16/scanner_page.h
@@ -1,14 +1,15 @@
 #ifndef _SCANNER_PAGE_H_
 #define _SCANNER_PAGE_H_
 
-#define MIN_RADIOCHANNEL     0x04
-#define MAX_RADIOCHANNEL     0x54
+#define MIN_RADIOCHANNEL     0x00
+#define MAX_RADIOCHANNEL     0x4F
 
 struct scanner_page {
-    u8 channelnoise[MAX_RADIOCHANNEL - MIN_RADIOCHANNEL];
+    u8 channelnoise[MAX_RADIOCHANNEL - MIN_RADIOCHANNEL + 1];
     u8 channel;
     u8 time_to_scan;
     u8 enable;
     u8 scan_mode;
+    u8 attenuator;
 };
 #endif
