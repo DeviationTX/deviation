2c4c922db804a395391c3ab53eee85c3e86d8a02
hexfet <jpublic123@yahoo.com>
2016-10-16 21:09:47 -0400

    Set at9 and utx uart support to import from common/devo so changes only have to be made in one place.

--- src/universaltx/utx_uart.c
+++ src/universaltx/utx_uart.c
@@ -21,29 +21,5 @@
 #include <string.h>
 #include <errno.h>
 
-void UART_Initialize()
-{
-    /* Enable clocks for GPIO port A (for GPIO_USART1_TX) and USART1. */
-    rcc_periph_clock_enable(RCC_GPIOA);
-    rcc_periph_clock_enable(RCC_USART4);
 
-    /* Setup GPIO pin PA.0 for transmit. */
-    gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO0);
-    gpio_set_af(GPIOA, GPIO_AF4, GPIO0); // AF4 ==> USART4
-
-    /* Setup UART parameters. */
-    usart_set_baudrate(USART4, 115200);
-    usart_set_databits(USART4, 8);
-    usart_set_stopbits(USART4, USART_CR2_STOP_1_0BIT);
-    usart_set_mode(USART4, USART_MODE_TX);
-    usart_set_parity(USART4, USART_PARITY_NONE);
-    usart_set_flow_control(USART4, USART_FLOWCONTROL_NONE);
-
-    /* Finally enable the USART. */
-    usart_enable(USART4);
-}
-
-void UART_Stop()
-{
-    usart_disable(USART4);
-}
+#include "target/common/devo/uart.c"
