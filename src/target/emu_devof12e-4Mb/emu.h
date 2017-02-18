dea90e54f1ce013becc91f814d907953635b2de1
vladislavy <vladislavy@rambler.ru>
2017-02-18 13:40:02 +0300

    Add "Ultimate Devo F12E" (devof12e-4Mb)

---
+++ src/target/emu_devof12e-4Mb/emu.h
@@ -0,0 +1,12 @@
+#ifndef _EMU_H_
+#define _EMU_H_
+
+#include "lcd.h"
+
+#define PIXELS_PER_LOGICAL_X  CHAR_WIDTH
+#define PIXELS_PER_LOGICAL_Y  CHAR_HEIGHT
+#define ZOOM_X          1 / 2
+#define ZOOM_Y          1 / 2
+#define EMU_STRING "DEVO-F12E-4Mb-Emu"
+#define HAS_LCD_INIT    1
+#endif
