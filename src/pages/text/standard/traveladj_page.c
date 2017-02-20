0287eb18c973212089f10e809f2e94e5ca3c3cf7
vladislavy <vladislavy@rambler.ru>
2017-02-20 16:32:29 +0300

    Fix "text" GUI "Standard" - "Travel Adjust" page

--- src/pages/text/standard/traveladj_page.c
+++ src/pages/text/standard/traveladj_page.c
@@ -18,60 +18,15 @@
 #include "gui/gui.h"
 #include "config/model.h"
 #include "standard.h"
-#if HAS_STANDARD_GUI
-#include "../../common/standard/_traveladj_page.c"
 
-static struct stdtravel_obj * const gui = &gui_objs.u.stdtravel;
-
-static unsigned _action_cb(u32 button, unsigned flags, void *data);
-
-static u16 current_selected = 0;
-
-static int row_cb(int absrow, int relrow, int y, void *data)
-{
-    (void)data;
-    u8 w = 35;
-    u8 x = 50;
-    mp->limit = MIXER_GetLimit(absrow);
-    GUI_CreateLabelBox(&gui->chan[relrow], 0, y,
-            0, ITEM_HEIGHT, &DEFAULT_FONT, STDMIX_channelname_cb, NULL, (void *)(long)absrow);
-    GUI_CreateTextSelectPlate(&gui->dn[relrow], x, y,
-            w, ITEM_HEIGHT, &DEFAULT_FONT, NULL, traveldown_cb, (void *)(long)absrow);
-    GUI_CreateTextSelectPlate(&gui->up[relrow], x + w +3, y,
-            w, ITEM_HEIGHT, &DEFAULT_FONT, NULL, travelup_cb, (void *)(long)absrow);
-    return 2;
-}
-
-void PAGE_TravelAdjInit(int page)
-{
-    (void)page;
-    PAGE_SetActionCB(_action_cb);
-    PAGE_SetModal(0);
-    PAGE_RemoveAllObjects();
-
-    PAGE_ShowHeader(("")); // draw a underline only
-    u8 w = 35;
-    u8 x = 50;
-    GUI_CreateLabelBox(&gui->dnlbl, x+2, 0,  w, ITEM_HEIGHT, &DEFAULT_FONT, NULL, NULL, _tr("Down"));
-    GUI_CreateLabelBox(&gui->uplbl, x + w +5, 0,  w, ITEM_HEIGHT, &DEFAULT_FONT, NULL, NULL, _tr("Up"));
-
-    GUI_CreateScrollable(&gui->scrollable, 0, ITEM_HEIGHT + 1, LCD_WIDTH, LCD_HEIGHT - ITEM_HEIGHT -1,
-                         ITEM_SPACE, Model.num_channels, row_cb, NULL, NULL, NULL);
-    PAGE_SetScrollable(&gui->scrollable, &current_selected);
-}
-
-static unsigned _action_cb(u32 button, unsigned flags, void *data)
-{
-    (void)data;
-    if ((flags & BUTTON_PRESS) || (flags & BUTTON_LONGPRESS)) {
-        if (CHAN_ButtonIsPressed(button, BUT_EXIT)) {
-            PAGE_ChangeByID(PAGEID_MENU, PREVIOUS_ITEM);
-        }
-        else {
-            // only one callback can handle a button press, so we don't handle BUT_ENTER here, let it handled by press cb
-            return 0;
-        }
-    }
-    return 1;
-}
-#endif //HAS_STANDARD_GUI
+#define OVERRIDE_PLACEMENT
+enum {
+    LABEL_X        = 8*ITEM_SPACE,
+    LABEL_WIDTH    = 4*ITEM_SPACE,
+    LABEL_OFFSET   = 4*ITEM_SPACE,
+    HEADER_X       = 8*ITEM_SPACE,
+    HEADER_WIDTH   = 6*ITEM_SPACE,
+    HEADER_OFFSET1 = ITEM_SPACE,
+    HEADER_OFFSET2 = 3*ITEM_SPACE,
+};
+#include "../../128x64x1/standard/traveladj_page.c"
