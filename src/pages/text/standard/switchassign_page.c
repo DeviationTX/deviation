c6c88c10cd3ca7cb6fd8d6e5caff19c3181aeb9c
vladislavy <vladislavy@rambler.ru>
2017-02-21 10:21:32 +0300

    Fix "text" GUI "Standard" - "Gyrosense", "Switch assignment" and "Swash" pages

--- src/pages/text/standard/switchassign_page.c
+++ src/pages/text/standard/switchassign_page.c
@@ -19,79 +19,9 @@
 #include "config/model.h"
 #include "standard.h"
 
-#if HAS_STANDARD_GUI
-#include "../../common/standard/_switchassign_page.c"
-
-static unsigned _action_cb(u32 button, unsigned flags, void *data);
-
-static const char *label_cb(guiObject_t *obj, const void *data)
-{
-    (void)obj;
-    FunctionSwitch i = (long)data;
-    switch (i) {
-    case SWITCHFUNC_FLYMODE:
-        strcpy(tempstring, _tr("Fly mode"));
-        break;
-    case SWITCHFUNC_HOLD:
-        strcpy(tempstring, _tr("Thr hold"));
-        break;
-    case SWITCHFUNC_GYROSENSE:
-        strcpy(tempstring, _tr("Gyro sense"));
-        break;
-    case SWITCHFUNC_DREXP_AIL:
-        strcpy(tempstring, _tr("D/R&Exp -AIL"));
-        break;
-    case SWITCHFUNC_DREXP_ELE:
-        strcpy(tempstring, _tr("D/R&Exp -ELE"));
-        break;
-    case SWITCHFUNC_DREXP_RUD:
-    default:
-        strcpy(tempstring, _tr("D/R&Exp -RUD"));
-        break;
-    }
-    return tempstring;
-}
-
-static int row_cb(int absrow, int relrow, int y, void *data)
-{
-    (void)data;
-    u8 w = 46;
-    u8 x = 77;
-    GUI_CreateLabelBox(&gui->name[relrow], 0, y, 0, ITEM_HEIGHT, &DEFAULT_FONT, label_cb, NULL, (void *)(long)absrow);
-    GUI_CreateTextSelectPlate(&gui->value[relrow], x, y,
-        w, ITEM_HEIGHT, &DEFAULT_FONT, NULL, switch_cb, (void *)(long)absrow);
-    return 1;
-}
-
-void PAGE_SwitchAssignInit(int page)
-{
-    (void)page;
-    PAGE_SetActionCB(_action_cb);
-    PAGE_SetModal(0);
-    PAGE_RemoveAllObjects();
-    refresh_switches();
-
-    PAGE_ShowHeader(_tr("Press ENT to change"));
-    GUI_CreateScrollable(&gui->scrollable, 0, ITEM_HEIGHT + 1, LCD_WIDTH, LCD_HEIGHT - ITEM_HEIGHT -1,
-                     ITEM_SPACE, SWITCHFUNC_LAST, row_cb, NULL, NULL, NULL);
-    GUI_SetSelected(GUI_ShowScrollableRowOffset(&gui->scrollable, 0));
-}
-
-static unsigned _action_cb(u32 button, unsigned flags, void *data)
-{
-    (void)data;
-    if ((flags & BUTTON_PRESS) || (flags & BUTTON_LONGPRESS)) {
-        if (CHAN_ButtonIsPressed(button, BUT_EXIT)) {
-            PAGE_ChangeByID(PAGEID_MENU, PREVIOUS_ITEM);
-        } else if (CHAN_ButtonIsPressed(button, BUT_ENTER)) {
-            MUSIC_Play(MUSIC_SAVING);
-            save_changes();
-        }
-        else {
-            // only one callback can handle a button press, so we don't handle BUT_ENTER here, let it handled by press cb
-            return 0;
-        }
-    }
-    return 1;
-}
-#endif //HAS_STANDARD_GUI
+#define OVERRIDE_PLACEMENT
+enum {
+    LABEL_X        = 16*ITEM_SPACE,
+    LABEL_WIDTH    = 7*ITEM_SPACE,
+};
+#include "../../128x64x1/standard/switchassign_page.c"
