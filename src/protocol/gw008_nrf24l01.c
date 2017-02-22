ded703652ddc42a303653e463318a3423d4d1bf8
Goebish <goebish@gmail.com>
2017-02-22 23:41:45 +0100

    Grab rxid

--- src/protocol/gw008_nrf24l01.c
+++ src/protocol/gw008_nrf24l01.c
@@ -41,10 +41,8 @@
 
 #ifdef EMULATOR
     #define USE_FIXED_MFGID
-    #define BIND_COUNT 4
     #define dbgprintf printf
 #else
-    #define BIND_COUNT 1000
     //printf inside an interrupt handler is really dangerous
     //this shouldn't be enabled even in debug builds without explicitly
     //turning it on
@@ -60,8 +58,9 @@ static u8 packet[PAYLOAD_SIZE];
 static u8 rf_chans[4] = {0x32, 0x3f, 0x42, 0x47};
 static u8 current_chan;
 static u8 phase;
-static u16 bind_counter;
 static u8 tx_power;
+static u8 rxid;
+static u8 txid[2] = {0x38, 0xf6};
 
 enum {
     BIND,
@@ -99,16 +98,22 @@ static u16 scale_channel(u8 ch, u16 destMin, u16 destMax)
 
 static void send_packet(u8 bind)
 {
+    packet[0] = txid[0];
     if(bind) {
-        memcpy(packet, (u8*)"\x38\x55\x32\x3f\x42\x47\x00\x00\x00\x00\x00\x00\x00\xaa\xf6", PAYLOAD_SIZE);
+        packet[1] = 0x55;
+        packet[2] = rf_chans[0];
+        packet[3] = rf_chans[1];
+        packet[4] = rf_chans[2];
+        packet[5] = rf_chans[3];
+        memset(&packet[6], 0, 7);
+        packet[13] = 0xaa;
     }
     else {
-        packet[0] = 0x38;
-        packet[1] = 0x01;
-        packet[2] = scale_channel(CHANNEL1, 200, 0);
-        packet[3] = scale_channel(CHANNEL2, 0, 200);
-        packet[4] = scale_channel(CHANNEL4, 200, 0);
-        packet[5] = scale_channel(CHANNEL3, 0, 200);
+        packet[1] = 0x01 | GET_FLAG(CHANNEL6, 0x40); // flip
+        packet[2] = scale_channel(CHANNEL1, 200, 0); // aileron
+        packet[3] = scale_channel(CHANNEL2, 0, 200); // elevator
+        packet[4] = scale_channel(CHANNEL4, 200, 0); // rudder
+        packet[5] = scale_channel(CHANNEL3, 0, 200); // throttle
         packet[6] = 0xaa;
         packet[7] = 0x02; // max rate
         packet[8] = 0x00;
@@ -116,9 +121,10 @@ static void send_packet(u8 bind)
         packet[10]= 0x00;
         packet[11]= 0x00;
         packet[12]= 0x00;
-        packet[13]= 0x18;
-        packet[14]= 0xf6;
+        packet[13]= rxid;
     }
+    packet[14] = txid[1];
+    
     // Power on, TX mode, CRC enabled
     XN297_Configure(BV(NRF24L01_00_EN_CRC) | BV(NRF24L01_00_CRCO) | BV(NRF24L01_00_PWR_UP));
     NRF24L01_WriteReg(NRF24L01_05_RF_CH, bind ? RF_BIND_CHANNEL : rf_chans[(current_chan++)/2]);
@@ -140,11 +146,13 @@ static void gw008_init()
     NRF24L01_Initialize();
     NRF24L01_SetTxRxMode(TX_EN);
     XN297_SetTXAddr((u8*)"\xcc\xcc\xcc\xcc\xcc", 5);
+    XN297_SetRXAddr((u8*)"\xcc\xcc\xcc\xcc\xcc", 5);
     NRF24L01_FlushTx();
     NRF24L01_FlushRx();
     NRF24L01_WriteReg(NRF24L01_07_STATUS, 0x70);     // Clear data ready, data sent, and retransmit
     NRF24L01_WriteReg(NRF24L01_01_EN_AA, 0x00);      // No Auto Acknowldgement on all data pipes
     NRF24L01_WriteReg(NRF24L01_02_EN_RXADDR, 0x01);
+    NRF24L01_WriteReg(NRF24L01_11_RX_PW_P0, PAYLOAD_SIZE+2); // pcf + payload
     NRF24L01_WriteReg(NRF24L01_03_SETUP_AW, 0x03);
     NRF24L01_WriteReg(NRF24L01_04_SETUP_RETR, 0x00); // no retransmits
     NRF24L01_SetBitrate(NRF24L01_BR_1M);
@@ -189,12 +197,27 @@ static u16 gw008_callback()
 {
     switch(phase) {
         case BIND:
-            send_packet(1);
-            bind_counter--;
-            if(bind_counter == 0) {
-                phase = DATA;
+            if((NRF24L01_ReadReg(NRF24L01_07_STATUS) & BV(NRF24L01_07_RX_DR)) &&   // RX fifo data ready
+                XN297_ReadEnhancedPayload(packet, PAYLOAD_SIZE) == PAYLOAD_SIZE && // check payload size
+                packet[0] == txid[0] && packet[14] == txid[1]) { // check tx id
+                NRF24L01_SetTxRxMode(TXRX_OFF);
+                NRF24L01_SetTxRxMode(TX_EN);
+                rxid = packet[13];
                 PROTOCOL_SetBindState(0);
+                phase = DATA;
+            } else {
+                NRF24L01_SetTxRxMode(TXRX_OFF);
+                NRF24L01_SetTxRxMode(TX_EN);
+                send_packet(1);
+                usleep(300);
+                // switch to RX mode
+                NRF24L01_SetTxRxMode(TXRX_OFF);
+                NRF24L01_FlushRx();
+                NRF24L01_SetTxRxMode(RX_EN);
+                XN297_Configure(BV(NRF24L01_00_EN_CRC) | BV(NRF24L01_00_CRCO) 
+                              | BV(NRF24L01_00_PWR_UP) | BV(NRF24L01_00_PRIM_RX)); 
             }
+            return 5000;
             break;
         case DATA:
             send_packet(0);
@@ -208,10 +231,9 @@ static void initialize()
     CLOCK_StopTimer();
     tx_power = Model.tx_power;
     phase = BIND;
-    bind_counter = BIND_COUNT;
     gw008_init();
     current_chan = 0;
-    PROTOCOL_SetBindState(BIND_COUNT * PACKET_PERIOD / 1000);
+    PROTOCOL_SetBindState(0xffffffff);
     CLOCK_StartTimer(INITIAL_WAIT, gw008_callback);
 }
 
