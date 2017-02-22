b9408641ca9a4ffe6c62aaa663ea3ef84df1d0cf
Goebish <goebish@gmail.com>
2017-02-22 01:10:41 +0100

    Add GW008 protocol

---
+++ src/protocol/gw008_nrf24l01.c
@@ -0,0 +1,231 @@
+/*
+ This project is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ Deviation is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with Deviation.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+#ifdef MODULAR
+  //Allows the linker to properly relocate
+  #define GW008_Cmds PROTO_Cmds
+  #pragma long_calls
+#endif
+#include "common.h"
+#include "interface.h"
+#include "mixer.h"
+#include "config/model.h"
+#include "config/tx.h" // for Transmitter
+#include "music.h"
+#include "telemetry.h"
+
+#ifdef MODULAR
+  //Some versions of gcc applythis to definitions, others to calls
+  //So just use long_calls everywhere
+  //#pragma long_calls_off
+  extern unsigned _data_loadaddr;
+  const unsigned long protocol_type = (unsigned long)&_data_loadaddr;
+#endif
+
+#ifdef PROTO_HAS_NRF24L01
+
+#include "iface_nrf24l01.h"
+
+#ifdef EMULATOR
+    #define USE_FIXED_MFGID
+    #define BIND_COUNT 4
+    #define dbgprintf printf
+#else
+    #define BIND_COUNT 1000
+    //printf inside an interrupt handler is really dangerous
+    //this shouldn't be enabled even in debug builds without explicitly
+    //turning it on
+    #define dbgprintf if(0) printf
+#endif
+
+#define INITIAL_WAIT    500
+#define PACKET_PERIOD   2400
+#define RF_BIND_CHANNEL 2
+#define PAYLOAD_SIZE    15
+
+static u8 packet[PAYLOAD_SIZE];
+static u8 rf_chans[4] = {0x32, 0x3f, 0x42, 0x47};
+static u8 current_chan;
+static u8 phase;
+static u16 bind_counter;
+static u8 tx_power;
+
+enum {
+    BIND,
+    DATA
+};
+
+// For code readability
+enum {
+    CHANNEL1 = 0,   // Aileron
+    CHANNEL2,       // Elevator
+    CHANNEL3,       // Throttle
+    CHANNEL4,       // Rudder
+    CHANNEL5,       // 
+    CHANNEL6,       // Flip
+};
+
+#define GET_FLAG(ch, mask) (Channels[ch] > 0 ? mask : 0)
+
+// Bit vector from bit position
+#define BV(bit) (1 << bit)
+
+#define CHAN_RANGE (CHAN_MAX_VALUE - CHAN_MIN_VALUE)
+
+static u16 scale_channel(u8 ch, u16 destMin, u16 destMax)
+{
+    s32 chanval = Channels[ch];
+    s32 range = (s32) destMax - (s32) destMin;
+
+    if (chanval < CHAN_MIN_VALUE)
+        chanval = CHAN_MIN_VALUE;
+    else if (chanval > CHAN_MAX_VALUE)
+        chanval = CHAN_MAX_VALUE;
+    return (range * (chanval - CHAN_MIN_VALUE)) / CHAN_RANGE + destMin;
+}
+
+static void send_packet(u8 bind)
+{
+    if(bind) {
+        memcpy(packet, (u8*)"\x38\x55\x32\x3f\x42\x47\x00\x00\x00\x00\x00\x00\x00\xaa\xf6", PAYLOAD_SIZE);
+    }
+    else {
+        packet[0] = 0x38;
+        packet[1] = 0x01;
+        packet[2] = scale_channel(CHANNEL1, 200, 0);
+        packet[3] = scale_channel(CHANNEL2, 0, 200);
+        packet[4] = scale_channel(CHANNEL4, 200, 0);
+        packet[5] = scale_channel(CHANNEL3, 0, 200);
+        packet[6] = 0xaa;
+        packet[7] = 0x02; // max rate
+        packet[8] = 0x00;
+        packet[9] = 0x00;
+        packet[10]= 0x00;
+        packet[11]= 0x00;
+        packet[12]= 0x00;
+        packet[13]= 0x18;
+        packet[14]= 0xf6;
+    }
+    // Power on, TX mode, CRC enabled
+    XN297_Configure(BV(NRF24L01_00_EN_CRC) | BV(NRF24L01_00_CRCO) | BV(NRF24L01_00_PWR_UP));
+    NRF24L01_WriteReg(NRF24L01_05_RF_CH, bind ? RF_BIND_CHANNEL : rf_chans[(current_chan++)/2]);
+    current_chan %= 8;
+
+    NRF24L01_WriteReg(NRF24L01_07_STATUS, 0x70);
+    NRF24L01_FlushTx();
+    XN297_WriteEnhancedPayload(packet, PAYLOAD_SIZE, 0, 0x3c7d);
+}
+
+static void gw008_init()
+{
+    NRF24L01_Initialize();
+    NRF24L01_SetTxRxMode(TX_EN);
+    XN297_SetTXAddr((u8*)"\xcc\xcc\xcc\xcc\xcc", 5);
+    NRF24L01_FlushTx();
+    NRF24L01_FlushRx();
+    NRF24L01_WriteReg(NRF24L01_07_STATUS, 0x70);     // Clear data ready, data sent, and retransmit
+    NRF24L01_WriteReg(NRF24L01_01_EN_AA, 0x00);      // No Auto Acknowldgement on all data pipes
+    NRF24L01_WriteReg(NRF24L01_02_EN_RXADDR, 0x01);
+    NRF24L01_WriteReg(NRF24L01_03_SETUP_AW, 0x03);
+    NRF24L01_WriteReg(NRF24L01_04_SETUP_RETR, 0x00); // no retransmits
+    NRF24L01_SetBitrate(NRF24L01_BR_1M);
+    NRF24L01_SetPower(Model.tx_power);
+    NRF24L01_Activate(0x73);                         // Activate feature register
+    NRF24L01_WriteReg(NRF24L01_1C_DYNPD, 0x00);      // Disable dynamic payload length on all pipes
+    NRF24L01_WriteReg(NRF24L01_1D_FEATURE, 0x01);    // Set feature bits on
+    NRF24L01_Activate(0x73);
+    
+    // Check for Beken BK2421/BK2423 chip
+    // It is done by using Beken specific activate code, 0x53
+    // and checking that status register changed appropriately
+    // There is no harm to run it on nRF24L01 because following
+    // closing activate command changes state back even if it
+    // does something on nRF24L01
+    NRF24L01_Activate(0x53); // magic for BK2421 bank switch
+    dbgprintf("Trying to switch banks\n");
+    if (NRF24L01_ReadReg(NRF24L01_07_STATUS) & 0x80) {
+        dbgprintf("BK2421 detected\n");
+        // Beken registers don't have such nice names, so we just mention
+        // them by their numbers
+        // It's all magic, eavesdropped from real transfer and not even from the
+        // data sheet - it has slightly different values
+        NRF24L01_WriteRegisterMulti(0x00, (u8 *) "\x40\x4B\x01\xE2", 4);
+        NRF24L01_WriteRegisterMulti(0x01, (u8 *) "\xC0\x4B\x00\x00", 4);
+        NRF24L01_WriteRegisterMulti(0x02, (u8 *) "\xD0\xFC\x8C\x02", 4);
+        NRF24L01_WriteRegisterMulti(0x03, (u8 *) "\x99\x00\x39\x21", 4);
+        NRF24L01_WriteRegisterMulti(0x04, (u8 *) "\xD9\x96\x82\x1B", 4);
+        NRF24L01_WriteRegisterMulti(0x05, (u8 *) "\x24\x06\x7F\xA6", 4);
+        NRF24L01_WriteRegisterMulti(0x0C, (u8 *) "\x00\x12\x73\x00", 4);
+        NRF24L01_WriteRegisterMulti(0x0D, (u8 *) "\x46\xB4\x80\x00", 4);
+        NRF24L01_WriteRegisterMulti(0x04, (u8 *) "\xDF\x96\x82\x1B", 4);
+        NRF24L01_WriteRegisterMulti(0x04, (u8 *) "\xD9\x96\x82\x1B", 4);
+    } else {
+        dbgprintf("nRF24L01 detected\n");
+    }
+    NRF24L01_Activate(0x53); // switch bank back
+}
+
+MODULE_CALLTYPE
+static u16 gw008_callback()
+{
+    switch(phase) {
+        case BIND:
+            send_packet(1);
+            bind_counter--;
+            if(bind_counter == 0) {
+                phase = DATA;
+                PROTOCOL_SetBindState(0);
+            }
+            break;
+        case DATA:
+            send_packet(0);
+            break;
+    }
+    return PACKET_PERIOD;
+}
+
+static void initialize()
+{
+    CLOCK_StopTimer();
+    tx_power = Model.tx_power;
+    phase = BIND;
+    bind_counter = BIND_COUNT;
+    gw008_init();
+    current_chan = 0;
+    PROTOCOL_SetBindState(BIND_COUNT * PACKET_PERIOD / 1000);
+    CLOCK_StartTimer(INITIAL_WAIT, gw008_callback);
+}
+
+const void *GW008_Cmds(enum ProtoCmds cmd)
+{
+    switch(cmd) {
+        case PROTOCMD_INIT:  initialize(); return 0;
+        case PROTOCMD_DEINIT:
+        case PROTOCMD_RESET:
+            CLOCK_StopTimer();
+            return (void *)(NRF24L01_Reset() ? 1L : -1L);
+        case PROTOCMD_CHECK_AUTOBIND: return (void *)1L; // always Autobind
+        case PROTOCMD_BIND:  initialize(); return 0;
+        case PROTOCMD_NUMCHAN: return (void *) 6L; // A, E, T, R, N/A, flip
+        case PROTOCMD_DEFAULT_NUMCHAN: return (void *)6L;
+        case PROTOCMD_CURRENT_ID: return Model.fixed_id ? (void *)((unsigned long)Model.fixed_id) : 0;
+        case PROTOCMD_GETOPTIONS: return (void*)0L;
+        case PROTOCMD_TELEMETRYSTATE: return (void *)(long)PROTO_TELEM_UNSUPPORTED;
+        default: break;
+    }
+    return 0;
+}
+#endif
