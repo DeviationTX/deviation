d9a0abd386f91a70691601461f39d09b42b5dc0d
TheRealMoeder <canofspam@web.de>
2017-07-01 21:40:01 +0200

    Revert "Voice for devo7e test"
    This reverts commit 4769c92464d3f36d7bd869a914927f077b4e37d0.

--- src/pages/common/_telemconfig_page.c
+++ src/pages/common/_telemconfig_page.c
@@ -114,7 +114,7 @@ static void sound_test_cb(guiObject_t *obj, void *data)
 {
     (void)obj;
     u8 idx = (long)data;
-#if HAS_EXTENDED_AUDIO
+#if HAS_EXTENDED_AUDIO && HAS_MUSIC_CONFIG
     MUSIC_Play(MUSIC_GetTelemetryAlarm(MUSIC_TELEMALARM1 + idx));
 #else
     MUSIC_Play(MUSIC_TELEMALARM1 + idx);
