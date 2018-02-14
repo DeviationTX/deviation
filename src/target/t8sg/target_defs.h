d9f5b0c964e11483ab225e70eb701b990afc084f
vladislavy <vladislavy@rambler.ru>
2018-02-14 09:23:25 +0300

    Disable 4IN1_FLASH support
    It has not sense for transmitter with integrated 4in1 module. Probably we can disable 4IN1_FLASH support for all transmitters with plenty flash memory.

--- src/target/t8sg/target_defs.h
+++ src/target/t8sg/target_defs.h
@@ -24,7 +24,7 @@
 #define HAS_EXTRA_POTS      OPTIONAL
 #define HAS_MULTIMOD_SUPPORT 1
 #define HAS_VIDEO           0
-#define HAS_4IN1_FLASH      1
+#define HAS_4IN1_FLASH      0
 #define HAS_EXTENDED_AUDIO  1
 #define HAS_AUDIO_UART5     1
 #define HAS_MUSIC_CONFIG    1
