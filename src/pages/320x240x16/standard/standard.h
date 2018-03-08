3e70ed70b351499fb87d59db920ab3baf8b4d469
vladislavy <vladislavy@rambler.ru>
2018-03-08 21:40:36 +0300

    Fix "ENTRIES_BY_SCREENSIZE" for 480x272x16 GUI

--- src/pages/320x240x16/standard/standard.h
+++ src/pages/320x240x16/standard/standard.h
@@ -3,7 +3,7 @@
 #include "../../common/standard/common_standard.h"
 
 int STDMIX_ScrollCB(guiObject_t *parent, u8 pos, s8 direction, void *data);
-#define ENTRIES_BY_SCREENSIZE (LCD_WIDTH == 480? 10 : 8) // 320x240: 8, 480x272: 10
+#define ENTRIES_BY_SCREENSIZE (LCD_WIDTH == 480 ? 9 : 8) // 320x240: 8, 480x272: 9
 #define ENTRIES_PER_PAGE      (ENTRIES_BY_SCREENSIZE > NUM_CHANNELS ? NUM_CHANNELS : ENTRIES_BY_SCREENSIZE)
 
 #endif
