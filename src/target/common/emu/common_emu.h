1aefd592d6dfe116a3dd97ad6b74c039d4f1b23d
PhracturedBlue <rc2012@pblue.org>
2019-01-12 10:43:01 -0800

    Remove dead code

--- src/target/common/emu/common_emu.h
+++ src/target/common/emu/common_emu.h
@@ -5,14 +5,6 @@
 #error "Don't include target_defs.h directly, include target.h instead."
 #endif
 
-//FAT is unused for the emulator
-//#if EMULATOR == USE_NATIVE_FS
-//    typedef struct {
-//        int dummy;
-//    }FATFS;
-//    #define fs_is_initialized(x) x
-//    #define fs_add_file_descriptor(x, y) (void)NULL
-//#endif
 //This is a trick to only enable this function in the emu and to use an inline version for devo
 #define LCD_ForceUpdate LCD_ForceUpdate
 #endif
