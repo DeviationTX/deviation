edcf51a45c76b0073a6d11a7206d13003e4644ec
Howard Su <howard0su@gmail.com>
2019-01-16 23:16:15 +0800

    Use right asm syntax for usleep (#596)
    * Tell the compiler we are using input parameter explicitly.
    * Add .n to be explicit on jump range

--- src/target/at9/tx_misc.c
+++ src/target/at9/tx_misc.c
@@ -3,13 +3,18 @@
 
 void _usleep(u32 x)
 {
-    (void)x;
-    asm ("mov r1, #24;"
+    asm volatile
+        ("mov r1, #24;"
          "mul r0, r0, r1;"
-         "b _delaycmp;"
+         "b.n _delaycmp;"
          "_delayloop:"
          "subs r0, r0, #1;"
          "_delaycmp:;"
          "cmp r0, #0;"
-         " bne _delayloop;");
+         "bne.n _delayloop;"
+         :
+         : "r" (x)
+         : "r1"
+         );
 }
+
--- src/target/common/devo/tx_misc.c
+++ src/target/common/devo/tx_misc.c
@@ -20,13 +20,17 @@ void TxName(u8 *var, int len)
 
 void _usleep(u32 x)
 {
-    (void)x;
-    asm ("mov r1, #24;"
+    asm volatile
+        ("mov r1, #24;"
          "mul r0, r0, r1;"
-         "b _delaycmp;"
+         "b.n _delaycmp;"
          "_delayloop:"
          "subs r0, r0, #1;"
          "_delaycmp:;"
          "cmp r0, #0;"
-         " bne _delayloop;");
+         "bne.n _delayloop;"
+         :
+         : "r" (x)
+         : "r1"
+         );
 }
