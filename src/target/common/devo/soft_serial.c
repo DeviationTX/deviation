5d184e395fc8af1a95ba05016bdc24f25047a82d
Howard Su <howard0su@gmail.com>
2019-01-16 12:36:50 +0800

    fix the build

--- src/target/common/devo/soft_serial.c
+++ src/target/common/devo/soft_serial.c
@@ -14,7 +14,7 @@
  */
 
 #include <libopencm3/cm3/nvic.h>
-#include <libopencm3/cm3/systick.h>
+#include <libopencm3/stm32/exti.h>
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/timer.h>
