911b86650c1c41eeb1fdd0f6ec5a03bc8f38ab6c
Howard Su <howard0su@gmail.com>
2019-01-16 07:51:58 +0000

    Add .n to explict on jump range

--- src/target/common/devo/tx_misc.c
+++ src/target/common/devo/tx_misc.c
@@ -23,12 +23,12 @@ void _usleep(u32 x)
     asm volatile
         ("mov r1, #24;"
          "mul r0, r0, r1;"
-         "b _delaycmp;"
+         "b.n _delaycmp;"
          "_delayloop:"
          "subs r0, r0, #1;"
          "_delaycmp:;"
          "cmp r0, #0;"
-         " bne _delayloop;"
+         "bne.n _delayloop;"
          :
          : "r" (x)
          : "r1"
