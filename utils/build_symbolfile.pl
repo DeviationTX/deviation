40d2bd7be846fd30f124c1fb717e948aaf90694b
Howard Su <howard0su@gmail.com>
2019-01-23 15:56:20 +0800

    Only export the functions with MODULE_CALLTYPE to module

--- utils/build_symbolfile.pl
+++ utils/build_symbolfile.pl
@@ -1,7 +1,27 @@
 #!/usr/bin/env perl
+use strict;
+use warnings;
+
+use Getopt::Long;
+
+my $exports_file = "";
+my %exports;
+
+GetOptions("exports=s" => \$exports_file);
+
+open my $fh, "<", $exports_file;
+while (<$fh>) {
+	chomp;
+	$exports{$_} = 1;
+}
+close($fh);
+
 while(<>) {
 	if(/^(\S+) g.....([FO])..* (\S+)/) {
-            printf "$3 = 0x%08x;\n", hex($1) + ($2 eq "O" ? 0 : 1);
+            # Either in export table or it is a data
+            if (exists($exports{$3}) || ($2 eq "O")) {
+                printf "$3 = 0x%08x;\n", hex($1) + ($2 eq "O" ? 0 : 1);
+            }
         } elsif(/^(\S+).*\*ABS\*\s+0+\s+_data_loadaddr/) {
             printf "_data_loadaddr = 0x%08x;\n", hex($1);
         }
