d24be12310d6c69794c877bb962effbadf410f26
PhracturedBlue <rc2012@pblue.org>
2019-01-23 05:22:29 -0800

    lint errors

--- src/tests/test_pages_advanced_mixer.c
+++ src/tests/test_pages_advanced_mixer.c
@@ -23,25 +23,25 @@ void TestMixerPageReorder(CuTest *t)
     reorder_return_cb(list);
 
     printf("%d %d\n", NUM_CHANNELS, NUM_OUT_CHANNELS+1);
-    
-    for(unsigned i = 0; i < 2*sizeof(order); i++) {
+
+    for (unsigned i = 0; i < 2*sizeof(order); i++) {
         unsigned dest = i / 2;
         unsigned src = 1 + (list[dest]-1) + (i % 2);
         printf("%d: src %d == %d   dest %d == %d\n", i, src, Model.mixers[i].src, dest, Model.mixers[i].dest);
     }
-    for(unsigned i = 0; i < 2*sizeof(order); i++) {
+    for (unsigned i = 0; i < 2*sizeof(order); i++) {
         unsigned dest = i / 2;
         unsigned src = 1 + (list[dest]-1) + (i % 2);
         CuAssertIntEquals(t, src, Model.mixers[i].src);
         CuAssertIntEquals(t, dest, Model.mixers[i].dest);
     }
 
-    for(unsigned i = 0; i < sizeof(order); i++) {
+    for (unsigned i = 0; i < sizeof(order); i++) {
         unsigned templ = list[i]-1 + MIXERTEMPLATE_CYC3 + 1;
         unsigned max = list[i]-1 < NUM_OUT_CHANNELS ? list[i]-1 : 150;
         printf("%d: templ %d == %d limit %d == %d\n", i, templ, Model.templates[i], max, Model.limits[i].max);
     }
-    for(unsigned i = 0; i < sizeof(order); i++) {
+    for (unsigned i = 0; i < sizeof(order); i++) {
         unsigned templ = list[i]-1 + MIXERTEMPLATE_CYC3 + 1;
         unsigned max = list[i]-1 < NUM_OUT_CHANNELS ? list[i]-1 : 150;
         CuAssertIntEquals(t, max, Model.limits[i].max);
