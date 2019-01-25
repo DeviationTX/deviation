DeviationTX/deviation

commit b7e1da190c4af053827f5c4b52b8e0cb05b789ae

Move PAGE_SetModal(0) to PAGE_ChangeByID

src/pages/128x64x1/model_page.c
@@ -149,7 +149,6 @@ static int row_cb(int absrow, int relrow, int y, void *data)
 void PAGE_ModelInit(int page)
 {
     (void)page;
-    PAGE_SetModal(0);
     memset(gui, 0, sizeof(struct modelpage_obj));
     mp->file_state = 0;
     mp->last_txpower = Model.tx_power;

src/pages/320x240x16/model_page.c
@@ -49,7 +49,6 @@ void PAGE_ModelInit(int page)
     mp->last_mixermode = Model.mixer_mode;
     mp->last_txpower = Model.tx_power;
     mp->file_state = 0;
-    PAGE_SetModal(0);
     PAGE_ShowHeader(PAGE_GetName(PAGEID_MODEL));
 
     enum {

src/pages/128x64x1/debuglog_page.c
@@ -33,7 +33,6 @@ void PAGE_DebuglogInit(int page)
 {
     (void)page;
     PAGE_ShowHeader(PAGE_GetName(PAGEID_DEBUGLOG));
-    PAGE_SetModal(0);
 
     find_line_ends();
     GUI_CreateScrollable(&gui->scrollable,

src/pages/128x64x1/main_page.c
@@ -66,7 +66,6 @@ void PAGE_MainInit(int page)
     TGLICO_LoadFonts();
     memset(mp, 0, sizeof(struct main_page));// Bug fix: must initialize this structure to avoid unpredictable issues in the PAGE_MainEvent
     memset(gui, 0, sizeof(struct mainpage_obj));
-    PAGE_SetModal(0);
     PAGE_SetActionCB(_action_cb);
     next_scan = CLOCK_getms()+BATTERY_SCAN_MSEC;
 

src/pages/128x64x1/standard/common_standard.c
@@ -58,7 +58,6 @@ static int row_cb(int absrow, int relrow, int y, void *data)
 
 void STANDARD_Init(const struct page_defs *page_defs)
 {
-    PAGE_SetModal(0);
     PAGE_RemoveAllObjects();
     PAGE_ShowHeader(_tr(page_defs->title));
     GUI_CreateScrollable(&gui->scrollable, 0, HEADER_HEIGHT, LCD_WIDTH, LCD_HEIGHT - HEADER_HEIGHT,

src/pages/128x64x1/standard/curves_page.c
@@ -88,7 +88,6 @@ static void press_cb(guiObject_t *obj, void *data)
 static void show_page(CurvesMode _curve_mode, int page)
 {
     (void)page;
-    PAGE_SetModal(0);
     memset(mp, 0, sizeof(*mp));
     curve_mode = _curve_mode;
     int count;

src/pages/128x64x1/standard/drexp_page.c
@@ -75,7 +75,6 @@ static int row_cb(int absrow, int relrow, int y, void *data)
 void PAGE_DrExpInit(int page)
 {
     (void)page;
-    PAGE_SetModal(0);
     // draw a underline only:
     GUI_CreateRect(&gui->rect, 0, HEADER_WIDGET_HEIGHT, LCD_WIDTH, 1, &DEFAULT_FONT);
     memset(mp, 0, sizeof(*mp));

src/pages/128x64x1/standard/gyrosense_page.c
@@ -39,7 +39,6 @@ enum {
 void PAGE_GyroSenseInit(int page)
 {
     (void)page;
-    PAGE_SetModal(0);
     memset(mp, 0, sizeof(*mp));
     int expected = INPUT_NumSwitchPos(mapped_std_channels.switches[SWITCHFUNC_GYROSENSE]);
     int count = STDMIX_GetMixers(mp->mixer_ptr, mapped_std_channels.aux2, GYROMIXER_COUNT);

commit ae6c86d965a6d61b178ac23f6ff00bed89220090

Fix modal tracking bug introduced in b7e1da19

src/pages/128x64x1/model_page.c
@@ -175,6 +175,7 @@ static void _changename_done_cb(guiObject_t *obj, void *data)  // devo8 doesn't 
         //Save model info here so it shows up on the model page
         CONFIG_SaveModelIfNeeded();
     }
+    PAGE_SetModal(0);
     PAGE_ModelInit(-1);
 }
 

src/pages/320x240x16/model_page.c
@@ -115,6 +115,7 @@ static void _changename_done_cb(guiObject_t *obj, void *data)
         //Save model info here so it shows up on the model page
         CONFIG_SaveModelIfNeeded();
     }
+    PAGE_SetModal(0);
     PAGE_ModelInit(0);
 }
 

src/pages/common/_model_page.c
@@ -94,6 +94,7 @@ static void fixedid_done_cb(guiObject_t *obj, void *data)
         Model.fixed_id = atoi(mp->fixed_id);
     }
     GUI_RemoveObj(obj);
+    PAGE_SetModal(0);
     PAGE_ModelInit(-1); // must be -1 for devo10 to get back to correct page
 }
 static void fixedid_cb(guiObject_t *obj, const void *data)