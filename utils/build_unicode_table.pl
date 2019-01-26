6774490d079b90d6a4ed85acb0d86fe3e5bb705c
PhracturedBlue <rc2012@pblue.org>
2019-01-25 19:29:44 -0800

    Update build_unicde_table.pl to support comparing tst to font file.  Add missing characters to 12 and 15px fonts.  Update 23 font with rebuilt (but identical) version

--- utils/build_unicode_table.pl
+++ utils/build_unicode_table.pl
@@ -4,21 +4,50 @@ use warnings;
 
 use utf8;
 use Encode;
+use Getopt::Long;
 
 binmode(STDOUT, ":utf8");
 
+my $font;
+my %fontchars;
+GetOptions("font=s" => \$font);
+
+if ($font) {
+    open my $fh, "<", $font;
+    while(<$fh>) {
+        if(/^ENCODING\s+(\d+)/) {
+            $fontchars{int($1)} = 1;
+        }
+    }
+}
+
 my %chars;
-while(<>) {
-    my $str = decode("UTF-8", $_);
-    foreach my $c (split(//, $str)) {
-        $chars{ord($c)} = $c
+for my $f (@ARGV) {
+    open my $fh, "<", $f;
+    my $line = 0;
+    while(<$fh>) {
+        $line++;
+        my $str = decode("UTF-8", $_);
+        $str =~ s/^\x{FEFF}//;
+        chomp($str);
+        foreach my $c (split(//, $str)) {
+            my $c_int = ord($c);
+            if (! $chars{$c_int}) {
+                if ($font && ! $fontchars{$c_int}) {
+                    printf "$f($line) Missing %#x ($c) from $str\n", $c_int;
+                }
+                $chars{ord($c)} = $c
+            }
+        }
     }
 }
 foreach (sort {$a <=> $b} keys(%chars)) {
+    next if ($font && $fontchars{$_});
     my $utf8 = encode("UTF-8", $chars{$_});
     my @u8;
     foreach my $c (split(//, $utf8)) {
         push @u8, sprintf("%02x", ord($c));
     }
+    print "Missing: " if ($font) ;
     printf "%#x: $chars{$_} (@u8)\n", $_;
 }
