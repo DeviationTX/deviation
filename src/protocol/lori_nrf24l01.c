e9099329ba0c0e37ecc6322d823f225756dec567
Goebish <goebish@gmail.com>
2019-02-01 01:17:42 +0100

    Add end of bind

--- src/protocol/lori_nrf24l01.c
+++ src/protocol/lori_nrf24l01.c
@@ -161,6 +161,7 @@ static u16 LORI_callback()
                 if(packet[0]=='O' && packet[1]=='K') {
                     NRF24L01_WriteRegisterMulti(NRF24L01_0A_RX_ADDR_P0, rx_tx_addr, 5);
                     NRF24L01_WriteRegisterMulti(NRF24L01_10_TX_ADDR, rx_tx_addr, 5);
+                    PROTOCOL_SetBindState(0);
                     phase = DATA1;
                     break;
                 }
