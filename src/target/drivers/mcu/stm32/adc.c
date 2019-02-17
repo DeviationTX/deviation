f469f302abfb4444e29ce54ea100fb5bacc1a2c0
PhracturedBlue <rc2012@pblue.org>
2019-02-17 06:20:38 -0800

    Cleanup build.  Move filesystem code

--- src/target/drivers/mcu/stm32/adc.c
+++ src/target/drivers/mcu/stm32/adc.c
@@ -16,7 +16,7 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/dma.h>
 #include "common.h"
-#include "../devo/devo.h"
+#include "target/common/devo/devo.h"
 #include <stdlib.h>
 #include <stdio.h>
 
