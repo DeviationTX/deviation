1053baddd175201f47b952dc27f289a9ff47eb8a
PhracturedBlue <rc2012@pblue.org>
2019-02-17 12:53:25 -0800

    More lint fixes

--- src/target/drivers/display/spi/128x64x1_nt7538.c
+++ src/target/drivers/display/spi/128x64x1_nt7538.c
@@ -91,10 +91,10 @@ void LCD_Init()
     LCD_Cmd(0xE2);  //Reset
     volatile int i = 0x8000;
     while(i) i--;
-    lcd_display(0);  // Display Off
-    LCD_Cmd(0xA6);   // Normal display
-    LCD_Cmd(0xA4);   // All Points Normal
-    LCD_Cmd(0xA0);   // Set SEG Direction (Normal)
+    lcd_display(0);     // Display Off
+    LCD_Cmd(0xA6);      // Normal display
+    LCD_Cmd(0xA4);      // All Points Normal
+    LCD_Cmd(0xA0);      // Set SEG Direction (Normal)
     if (HAS_LCD_FLIPPED) {
         LCD_Cmd(0xC8);  // Set COM Direction (Reversed)
         LCD_Cmd(0xA2);  // Set The LCD Display Driver Voltage Bias Ratio (1/9)
@@ -102,18 +102,18 @@ void LCD_Init()
         LCD_Cmd(0xEA);  // ??
         LCD_Cmd(0xC4);  // Common Output Mode Scan Rate
     }
-    LCD_Cmd(0x2C); // Power Controller:Booster ON
+    LCD_Cmd(0x2C);      // Power Controller:Booster ON
     i = 0x8000;
     while(i) i--;
-    LCD_Cmd(0x2E); // Power Controller: VReg ON
+    LCD_Cmd(0x2E);      // Power Controller: VReg ON
     i = 0x8000;
     while(i) i--;
-    LCD_Cmd(0x2F); // Power Controller: VFollower ON
+    LCD_Cmd(0x2F);      // Power Controller: VFollower ON
     i = 0x8000;
     while(i) i--;
     lcd_set_start_line(0);
     // Display data write (6)
-    //Clear the screen
+    // Clear the screen
     for(int page = 0; page < LCD_PAGES; page++) {
         lcd_set_page_address(page);
         lcd_set_column_address(0);
