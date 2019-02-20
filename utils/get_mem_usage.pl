9f4ca71f8d1049c4a458520245ec3a389b42e380
Howard Su <howard0su@gmail.com>
2019-02-21 00:27:02 +0800

    Print out stack size after link (#736)

--- utils/get_mem_usage.pl
+++ utils/get_mem_usage.pl
@@ -15,8 +15,11 @@ while(<>) {
         $rom_start = hex($1);
     } elsif(/(0x\S+)\s+_edata = \./) {
         $data_end = hex($1);
+    } elsif((/(0x\S+)\s+PROVIDE/)) {
+        $stack_end = hex($1);
     }
 }
 $rom_end += $data_end - $ram_start;
 printf "ROM: 0x%08x - 0x%08x = %6.2fkB\n", $rom_start, $rom_end, ($rom_end - $rom_start) / 1024.0;
 printf "RAM: 0x%08x - 0x%08x = %6.2fkB\n", $ram_start, $ram_end, ($ram_end - $ram_start) / 1024.0;
+printf "STK: 0x%08x - 0x%08x = %6.2fkB\n", $ram_end, $stack_end, ($stack_end - $ram_end) / 1024.0;
