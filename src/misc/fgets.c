DeviationTX/deviation

commit d9059767ca9b763a942cb89cb3e0970b66147559

Update FatFs.  Modify f_* calls to be FatFs compatible (#704)
* Update FatFs.  Modify f_* calls to be FatFs compatible
* Linter fixes

src/misc/fgets.c
@@ -19,12 +19,12 @@
 #if ! defined(EMULATOR) || EMULATOR == USE_INTERNAL_FS
 
 #define FILE void
-long _open_r (void *r, const char *file, int flags, int mode);
-int _close_r (void *r);
-int _read_r (void *r, char * ptr, int len);
-int _write_r (void *r, char * ptr, int len);
-int _lseek_r (void *r, int ptr, int dir);
-int _ltell_r (void *r);
+uintptr_t _open_r(void *r, const char *file, int flags, int mode);
+int _close_r(void *r);
+int _read_r(void *r, char * ptr, int len);
+int _write_r(void *r, char * ptr, int len);
+int _lseek_r(void *r, int ptr, int dir);
+int _ltell_r(void *r);
 int FS_Mount(void *FAT, const char *drive);
 
 FILE *devo_fopen2(void *r, const char *path, const char *mode)
@@ -32,7 +32,7 @@ FILE *devo_fopen2(void *r, const char *path, const char *mode)
     int flags = (mode && *mode == 'w') ? O_CREAT : 0;
     int _mode = flags ? O_WRONLY : O_RDONLY;
         
-    long fd = _open_r (r, path, flags, _mode);
+    uintptr_t fd = _open_r(r, path, flags, _mode);
     if (fd <= 0)
         return NULL;
     return (FILE *)fd;

src/target/drivers/filesystems/syscalls.c
@@ -35,6 +35,11 @@ static FATFS fat[2];
 static FATFS fat[1];
 #endif
 
+#ifdef FIL  // If FIL is #defined, it maps to FATFS
+    #define fil fat
+#else
+    static FIL fil[1];
+#endif
 extern u8 _drive_num;
 
 extern void init_err_handler();
@@ -42,13 +47,13 @@ extern void init_err_handler();
 int FS_Mount();
 void FS_Unmount();
 
-long _open_r (FATFS *r, const char *file, int flags, int mode);
-int _close_r (FATFS *r);
+uintptr_t _open_r(FIL *r, const char *file, int flags, int mode);
+int _close_r(FIL *r);
 
 
-int _read_r (FATFS *r, char * ptr, int len);
-int _write_r (FATFS *r, char * ptr, int len);
-int _lseek_r (FATFS *r, int ptr, int dir);
+int _read_r(FIL *r, char * ptr, int len);
+int _write_r(FIL *r, char * ptr, int len);
+int _lseek_r(FIL *r, int ptr, int dir);
 
 
 int FS_Mount(void *_f, const char *drive)
@@ -109,7 +114,9 @@ int FS_OpenDir(const char *path)
 int FS_ReadDir(char *path)
 {
     FILINFO fi;
-    if (fs_readdir(&dir, &fi) != FR_OK || ! fi.fname[0])
+    int res = fs_readdir(&dir, &fi);
+    dbgprintf("ReadDir: %d\n", res);
+    if (res != FR_OK || !fi.fname[0])
         return 0;
     dbgprintf("Read: %s %d\n", fi.fname, fi.fattrib);
     strlcpy(path, fi.fname, 13);
@@ -120,17 +127,17 @@ void FS_CloseDir()
 {
 }
 
-long _open_r (FATFS *r, const char *file, int flags, int mode) {
+uintptr_t _open_r(FIL *r, const char *file, int flags, int mode) {
     (void)flags;
     (void)mode;
 
     if(!r) {
 #ifdef MEDIA_DRIVE
         if (strncmp(file, "media/", 6) == 0)
-            r = &fat[1];
+            r = &fil[1];
         else
 #endif
-            r = &fat[0];
+            r = &fil[0];
     }
     if(fs_is_open(r)) {
         dbgprintf("_open_r(%p): file already open.\n", r);
@@ -138,8 +145,8 @@ long _open_r (FATFS *r, const char *file, int flags, int mode) {
     } else {
         fs_switchfile(r);
         _drive_num = fs_get_drive_num(r);
-        int res=fs_open(file, flags);
-        if(res==FR_OK) {
+        int res = fs_open(r, file, flags, mode);
+        if (res == FR_OK) {
             dbgprintf("_open_r(%08lx): fs_open (%s) flags: %d, mode: %d ok\r\n", r, file, flags, mode);
             if (flags & O_CREAT)
                 fs_maximize_file_size();
@@ -151,7 +158,7 @@ long _open_r (FATFS *r, const char *file, int flags, int mode) {
     }
 }
 
-int _close_r (FATFS *r) {
+int _close_r(FIL *r) {
     if(r) {
        fs_close(r);
        dbgprintf("_close_r(%p): file closed.\r\n", r);
@@ -159,68 +166,68 @@ int _close_r (FATFS *r) {
     return 0;
 }
 
-int _read_r (FATFS *r, char * ptr, int len)
+int _read_r(FIL *r, char * ptr, int len)
 {
     if((unsigned long)r>2 && fs_is_open(r)) {
         if(len <= 0xffff) {
-            u16  bytes_read;
+            unsigned  bytes_read = 0;
             fs_switchfile(r);
             _drive_num = fs_get_drive_num(r);
-            int res=fs_read(ptr, len, &bytes_read);
+            int res = fs_read(r, ptr, len, &bytes_read);
             dbgprintf("_read_r: len %d, bytes_read %d, result %d\r\n", len, bytes_read, res); 
-            if(res==FR_OK) return bytes_read;
+            if (res == FR_OK) return bytes_read;
         }
     }    
 
-    errno=EINVAL;
+    errno = EINVAL;
     return -1;
 }
 
-int _write_r (FATFS *r, char * ptr, int len)
+int _write_r(FIL *r, char * ptr, int len)
 {  
     if((unsigned long)r==1 || (unsigned long)r==2) {
         printstr(ptr, len);
         return len;
     } else if(r) {
         if(fs_is_open(r)) {
-            u16 bytes_written;
+            unsigned bytes_written = 0;
             fs_switchfile(r);
             _drive_num = fs_get_drive_num(r);
-            int res=fs_write(ptr, len, &bytes_written);
+            int res = fs_write(r, ptr, len, &bytes_written);
             dbgprintf("_write_r: len %d, bytes_written %d, result %d\r\n",len, bytes_written, res);
-            if(res==FR_OK) return bytes_written;
+            if (res == FR_OK) return bytes_written;
         }
     }
-    errno=EINVAL;
+    errno = EINVAL;
     return -1;
 }
 
-long _ltell_r (FATFS *r)
+int _ltell_r(FIL *r)
 {
-    if((unsigned long)r>2 && fs_is_open(r)) {
-        return fs_ltell(r);
+    if ((uintptr_t)r > 2 && fs_is_open(r)) {
+        return (int)fs_ltell(r);
     }
     return -1;
 }
 
-int _lseek_r (FATFS *r, int ptr, int dir)
+int _lseek_r(FIL *r, int ptr, int dir)
 {
     (void)r;
     
-    if((unsigned long)r>2 && fs_is_open(r)) {
-        if(dir==SEEK_CUR) {
+    if ((uintptr_t)r > 2 && fs_is_open(r)) {
+        if (dir == SEEK_CUR) {
             ptr += fs_ltell(r);
-        } else if (dir==SEEK_END) {
+        } else if (dir == SEEK_END) {
             ptr += fs_filesize(r);
         }
         fs_switchfile(r);
         _drive_num = fs_get_drive_num(r);
-        int res=fs_lseek(ptr);
-        if(res==FR_OK) {
+        int res = fs_lseek(r, ptr);
+        if (res == FR_OK) {
            return fs_ltell(r);
         }
     }
-    errno=EINVAL;
+    errno = EINVAL;
     return -1;
 }
-#endif //! defined(EMULATOR) || EMULATOR == USE_INTERNAL_FS
+#endif  // !defined(EMULATOR) || EMULATOR == USE_INTERNAL_FS

src/datalog.c
@@ -43,7 +43,7 @@ const u32 sample_rate[DLOG_RATE_LAST] = {
     [DLOG_RATE_1MIN]  = 60000,
 };
 
-static FATFS DatalogFAT;
+static FIL DatalogFAT;
 static FILE *fh;
 static u32 next_update;
 static u32 dlog_pos;

src/target/drivers/filesystems/FatFs/diskio.c
@@ -1,0 +1,225 @@
+/*-----------------------------------------------------------------------*/
+/* Low level disk I/O module skeleton for FatFs     (C)ChaN, 2016        */
+/*-----------------------------------------------------------------------*/
+/* If a working storage control module is available, it should be        */
+/* attached to the FatFs via a glue function rather than modifying it.   */
+/* This is an example of glue functions to attach various exsisting      */
+/* storage control modules to the FatFs module with a defined API.       */
+/*-----------------------------------------------------------------------*/
+
+#include "diskio.h"		/* FatFs lower layer API */
+
+/* Definitions of physical drive number for each drive */
+#define DEV_RAM		0	/* Example: Map Ramdisk to physical drive 0 */
+#define DEV_MMC		1	/* Example: Map MMC/SD card to physical drive 1 */
+#define DEV_USB		2	/* Example: Map USB MSD to physical drive 2 */
+
+
+/*-----------------------------------------------------------------------*/
+/* Get Drive Status                                                      */
+/*-----------------------------------------------------------------------*/
+
+DSTATUS disk_status (
+	BYTE pdrv		/* Physical drive nmuber to identify the drive */
+)
+{
+	DSTATUS stat;
+	int result;
+
+	switch (pdrv) {
+	case DEV_RAM :
+		result = RAM_disk_status();
+
+		// translate the reslut code here
+
+		return stat;
+
+	case DEV_MMC :
+		result = MMC_disk_status();
+
+		// translate the reslut code here
+
+		return stat;
+
+	case DEV_USB :
+		result = USB_disk_status();
+
+		// translate the reslut code here
+
+		return stat;
+	}
+	return STA_NOINIT;
+}
+
+
+
+/*-----------------------------------------------------------------------*/
+/* Inidialize a Drive                                                    */
+/*-----------------------------------------------------------------------*/
+
+DSTATUS disk_initialize (
+	BYTE pdrv				/* Physical drive nmuber to identify the drive */
+)
+{
+	DSTATUS stat;
+	int result;
+
+	switch (pdrv) {
+	case DEV_RAM :
+		result = RAM_disk_initialize();
+
+		// translate the reslut code here
+
+		return stat;
+
+	case DEV_MMC :
+		result = MMC_disk_initialize();
+
+		// translate the reslut code here
+
+		return stat;
+
+	case DEV_USB :
+		result = USB_disk_initialize();
+
+		// translate the reslut code here
+
+		return stat;
+	}
+	return STA_NOINIT;
+}
+
+
+
+/*-----------------------------------------------------------------------*/
+/* Read Sector(s)                                                        */
+/*-----------------------------------------------------------------------*/
+
+DRESULT disk_read (
+	BYTE pdrv,		/* Physical drive nmuber to identify the drive */
+	BYTE *buff,		/* Data buffer to store read data */
+	DWORD sector,	/* Start sector in LBA */
+	UINT count		/* Number of sectors to read */
+)
+{
+	DRESULT res;
+	int result;
+
+	switch (pdrv) {
+	case DEV_RAM :
+		// translate the arguments here
+
+		result = RAM_disk_read(buff, sector, count);
+
+		// translate the reslut code here
+
+		return res;
+
+	case DEV_MMC :
+		// translate the arguments here
+
+		result = MMC_disk_read(buff, sector, count);
+
+		// translate the reslut code here
+
+		return res;
+
+	case DEV_USB :
+		// translate the arguments here
+
+		result = USB_disk_read(buff, sector, count);
+
+		// translate the reslut code here
+
+		return res;
+	}
+
+	return RES_PARERR;
+}
+
+
+
+/*-----------------------------------------------------------------------*/
+/* Write Sector(s)                                                       */
+/*-----------------------------------------------------------------------*/
+
+DRESULT disk_write (
+	BYTE pdrv,			/* Physical drive nmuber to identify the drive */
+	const BYTE *buff,	/* Data to be written */
+	DWORD sector,		/* Start sector in LBA */
+	UINT count			/* Number of sectors to write */
+)
+{
+	DRESULT res;
+	int result;
+
+	switch (pdrv) {
+	case DEV_RAM :
+		// translate the arguments here
+
+		result = RAM_disk_write(buff, sector, count);
+
+		// translate the reslut code here
+
+		return res;
+
+	case DEV_MMC :
+		// translate the arguments here
+
+		result = MMC_disk_write(buff, sector, count);
+
+		// translate the reslut code here
+
+		return res;
+
+	case DEV_USB :
+		// translate the arguments here
+
+		result = USB_disk_write(buff, sector, count);
+
+		// translate the reslut code here
+
+		return res;
+	}
+
+	return RES_PARERR;
+}
+
+
+
+/*-----------------------------------------------------------------------*/
+/* Miscellaneous Functions                                               */
+/*-----------------------------------------------------------------------*/
+
+DRESULT disk_ioctl (
+	BYTE pdrv,		/* Physical drive nmuber (0..) */
+	BYTE cmd,		/* Control code */
+	void *buff		/* Buffer to send/receive control data */
+)
+{
+	DRESULT res;
+	int result;
+
+	switch (pdrv) {
+	case DEV_RAM :
+
+		// Process of the command for the RAM drive
+
+		return res;
+
+	case DEV_MMC :
+
+		// Process of the command for the MMC/SD card
+
+		return res;
+
+	case DEV_USB :
+
+		// Process of the command the USB drive
+
+		return res;
+	}
+
+	return RES_PARERR;
+}
+

src/target/drivers/filesystems/FatFs/diskio.h
@@ -1,5 +1,5 @@
-/*-----------------------------------------------------------------------
-/  Low level disk interface modlue include file   (C)ChaN, 2013
+/*-----------------------------------------------------------------------/
+/  Low level disk interface modlue include file   (C)ChaN, 2014          /
 /-----------------------------------------------------------------------*/
 
 #ifndef _DISKIO_DEFINED
@@ -8,9 +8,6 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-#define _USE_WRITE	1	/* 1: Enable disk_write function */
-#define _USE_IOCTL	1	/* 1: Enable disk_ioctl fucntion */
 
 #include "integer.h"
 
@@ -34,12 +31,13 @@ typedef enum {
 
 DSTATUS disk_initialize (BYTE pdrv);
 DSTATUS disk_status (BYTE pdrv);
-DRESULT disk_read (BYTE pdrv, BYTE*buff, DWORD sector, BYTE count);
-DRESULT disk_write (BYTE pdrv, const BYTE* buff, DWORD sector, BYTE count);
+DRESULT disk_read (BYTE pdrv, BYTE* buff, DWORD sector, UINT count);
+DRESULT disk_write (BYTE pdrv, const BYTE* buff, DWORD sector, UINT count);
 DRESULT disk_ioctl (BYTE pdrv, BYTE cmd, void* buff);
 
 
 /* Disk Status Bits (DSTATUS) */
+
 #define STA_NOINIT		0x01	/* Drive not initialized */
 #define STA_NODISK		0x02	/* No medium in the drive */
 #define STA_PROTECT		0x04	/* Write protected */
@@ -47,14 +45,14 @@ DRESULT disk_ioctl (BYTE pdrv, BYTE cmd, void* buff);
 
 /* Command code for disk_ioctrl fucntion */
 
-/* Generic command (used by FatFs) */
-#define CTRL_SYNC			0	/* Flush disk cache (for write functions) */
-#define GET_SECTOR_COUNT	1	/* Get media size (for only f_mkfs()) */
-#define GET_SECTOR_SIZE		2	/* Get sector size (for multiple sector size (_MAX_SS >= 1024)) */
-#define GET_BLOCK_SIZE		3	/* Get erase block size (for only f_mkfs()) */
-#define CTRL_ERASE_SECTOR	4	/* Force erased a block of sectors (for only _USE_ERASE) */
+/* Generic command (Used by FatFs) */
+#define CTRL_SYNC			0	/* Complete pending write process (needed at _FS_READONLY == 0) */
+#define GET_SECTOR_COUNT	1	/* Get media size (needed at _USE_MKFS == 1) */
+#define GET_SECTOR_SIZE		2	/* Get sector size (needed at _MAX_SS != _MIN_SS) */
+#define GET_BLOCK_SIZE		3	/* Get erase block size (needed at _USE_MKFS == 1) */
+#define CTRL_TRIM			4	/* Inform device that the data on the block of sectors is no longer used (needed at _USE_TRIM == 1) */
 
-/* Generic command (not used by FatFs) */
+/* Generic command (Not used by FatFs) */
 #define CTRL_POWER			5	/* Get/Set power status */
 #define CTRL_LOCK			6	/* Lock/Unlock media removal */
 #define CTRL_EJECT			7	/* Eject media */
@@ -66,20 +64,14 @@ DRESULT disk_ioctl (BYTE pdrv, BYTE cmd, void* buff);
 #define MMC_GET_CID			12	/* Get CID */
 #define MMC_GET_OCR			13	/* Get OCR */
 #define MMC_GET_SDSTAT		14	/* Get SD status */
+#define ISDIO_READ			55	/* Read data form SD iSDIO register */
+#define ISDIO_WRITE			56	/* Write data to SD iSDIO register */
+#define ISDIO_MRITE			57	/* Masked write data to SD iSDIO register */
 
 /* ATA/CF specific ioctl command */
 #define ATA_GET_REV			20	/* Get F/W revision */
 #define ATA_GET_MODEL		21	/* Get model name */
 #define ATA_GET_SN			22	/* Get serial number */
-
-
-/* MMC card type flags (MMC_GET_TYPE) */
-#define CT_MMC		0x01		/* MMC ver 3 */
-#define CT_SD1		0x02		/* SD ver 1 */
-#define CT_SD2		0x04		/* SD ver 2 */
-#define CT_SDC		(CT_SD1|CT_SD2)	/* SD */
-#define CT_BLOCK	0x08		/* Block addressing */
-
 
 #ifdef __cplusplus
 }

src/target/drivers/filesystems/FatFs/fattime.c
@@ -35,7 +35,7 @@
  */
 
 #include "integer.h"
-#include "../rtc.h"
+#include "../fs_rtc.h"
 
 DWORD get_fattime(void)
 {

commit e6580423e208a38c1c8f2eeb7693ecb9ef1abd45

Fix segfault in filehandling due to incorrect use of signed vs unsigned int (#726)
This fixes bugs introduced in d9059767ca9

src/misc/fgets.c
@@ -19,7 +19,7 @@
 #if ! defined(EMULATOR) || EMULATOR == USE_INTERNAL_FS
 
 #define FILE void
-uintptr_t _open_r(void *r, const char *file, int flags, int mode);
+intptr_t _open_r(void *r, const char *file, int flags, int mode);
 int _close_r(void *r);
 int _read_r(void *r, char * ptr, int len);
 int _write_r(void *r, char * ptr, int len);
@@ -32,7 +32,7 @@ FILE *devo_fopen2(void *r, const char *path, const char *mode)
     int flags = (mode && *mode == 'w') ? O_CREAT : 0;
     int _mode = flags ? O_WRONLY : O_RDONLY;
         
-    uintptr_t fd = _open_r(r, path, flags, _mode);
+    intptr_t fd = _open_r(r, path, flags, _mode);
     if (fd <= 0)
         return NULL;
     return (FILE *)fd;

src/target/drivers/filesystems/syscalls.c
@@ -47,7 +47,7 @@ extern void init_err_handler();
 int FS_Mount();
 void FS_Unmount();
 
-uintptr_t _open_r(FIL *r, const char *file, int flags, int mode);
+intptr_t _open_r(FIL *r, const char *file, int flags, int mode);
 int _close_r(FIL *r);
 
 
@@ -127,7 +127,7 @@ void FS_CloseDir()
 {
 }
 
-uintptr_t _open_r(FIL *r, const char *file, int flags, int mode) {
+intptr_t _open_r(FIL *r, const char *file, int flags, int mode) {
     (void)flags;
     (void)mode;
 
@@ -204,7 +204,7 @@ int _write_r(FIL *r, char * ptr, int len)
 
 int _ltell_r(FIL *r)
 {
-    if ((uintptr_t)r > 2 && fs_is_open(r)) {
+    if ((intptr_t)r > 2 && fs_is_open(r)) {
         return (int)fs_ltell(r);
     }
     return -1;
@@ -214,7 +214,7 @@ int _lseek_r(FIL *r, int ptr, int dir)
 {
     (void)r;
     
-    if ((uintptr_t)r > 2 && fs_is_open(r)) {
+    if ((intptr_t)r > 2 && fs_is_open(r)) {
         if (dir == SEEK_CUR) {
             ptr += fs_ltell(r);
         } else if (dir == SEEK_END) {