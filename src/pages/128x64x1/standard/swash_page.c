50e6c4812bbe7f5eededdaab3c43c9f62486cf4e
TheRealMoeder <canofspam@web.de>
2019-02-21 10:33:37 +0100

    Save ROM on 128x64 swash_page.c  (#682)
    * save ROM in swash_page.c
    
    * fix lint
    
    * move strings to array and use GUI_Localize
    
    * use uintptr_t

--- src/pages/128x64x1/standard/swash_page.c
+++ src/pages/128x64x1/standard/swash_page.c
@@ -43,16 +43,12 @@ void PAGE_SwashInit(int page)
     GUI_CreateTextSelectPlate(&gui->type, FIELD_X, 0, FIELD_WIDTH, HEADER_WIDGET_HEIGHT, &TEXTSEL_FONT, NULL, swash_val_cb, NULL); // FIXME: need a special value for header button/textsels
 
     u8 y = HEADER_HEIGHT + 1;
-    GUI_CreateLabelBox(&gui->lbl[0], LABEL_X, y, LABEL_WIDTH, LINE_HEIGHT, &LABEL_FONT, NULL, NULL, _tr("ELE Mix"));
-    GUI_CreateTextSelectPlate(&gui->mix[0], FIELD_X, y, FIELD_WIDTH, LINE_HEIGHT, &TEXTSEL_FONT, NULL, swashmix_val_cb, (void *)1);
-
-    y += LINE_SPACE;
-    GUI_CreateLabelBox(&gui->lbl[1], LABEL_X, y, LABEL_WIDTH, LINE_HEIGHT, &LABEL_FONT, NULL, NULL, _tr("AIL Mix"));
-    GUI_CreateTextSelectPlate(&gui->mix[1], FIELD_X, y, FIELD_WIDTH, LINE_HEIGHT, &TEXTSEL_FONT, NULL, swashmix_val_cb, (void *)0);
-
-    y += LINE_SPACE;
-    GUI_CreateLabelBox(&gui->lbl[2], LABEL_X, y, LABEL_WIDTH, LINE_HEIGHT, &LABEL_FONT, NULL, NULL, _tr("PIT Mix"));
-    GUI_CreateTextSelectPlate(&gui->mix[2], FIELD_X, y, FIELD_WIDTH, LINE_HEIGHT, &TEXTSEL_FONT, NULL, swashmix_val_cb, (void *)2);
+    const void * swashlbl[3] = { _tr_noop("ELE Mix"), _tr_noop("AIL Mix"), _tr_noop("PIT Mix") };
+    for (int i = 0; i < 3; i++) {
+        GUI_CreateLabelBox(&gui->lbl[i], LABEL_X, y, LABEL_WIDTH, LINE_HEIGHT, &LABEL_FONT, GUI_Localize, NULL, swashlbl[i]);
+        GUI_CreateTextSelectPlate(&gui->mix[i], FIELD_X, y, FIELD_WIDTH, LINE_HEIGHT, &TEXTSEL_FONT, NULL, swashmix_val_cb, (void *)(uintptr_t)i);
+        y += LINE_SPACE;
+    }
 
     update_swashmixes();
     GUI_Select1stSelectableObj();
