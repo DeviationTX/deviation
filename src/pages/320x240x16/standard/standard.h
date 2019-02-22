55d19377cf692b8671eb09afd2df9690da52e838
Howard Su <howard0su@gmail.com>
2019-02-23 00:48:21 +0800

    Fix crash on at10

--- src/pages/320x240x16/standard/standard.h
+++ src/pages/320x240x16/standard/standard.h
@@ -3,7 +3,7 @@
 #include "../../common/standard/common_standard.h"
 
 int STDMIX_ScrollCB(guiObject_t *parent, u8 pos, s8 direction, void *data);
-#define ENTRIES_BY_SCREENSIZE (LCD_HEIGHT / 30)  // 240: 8, 272: 9, 320: 10
+#define ENTRIES_BY_SCREENSIZE ((LCD_HEIGHT / 30) + 1)  // 240: 8, 272: 9, 320: 10
 #define ENTRIES_PER_PAGE      (ENTRIES_BY_SCREENSIZE > NUM_CHANNELS ? NUM_CHANNELS : ENTRIES_BY_SCREENSIZE)
 
 #endif
