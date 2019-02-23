DeviationTX/deviation

commit a097488aad51141a1816c679e5d5cd23ca61052a

Abstract ADC definitions (#750)
* Abstract ADC definitions
* Move analog pin defines into hardware.h
* move adc code into drivers/input

src/target/drivers/input/analog/analog.c
@@ -19,15 +19,61 @@
 #include "target/drivers/mcu/stm32/dma.h"
 #include "target/drivers/mcu/stm32/rcc.h"
 
-#include "target/tx/devo/common/devo.h"  // FIXME - Shouldn't rely on things from 'devo' here
+#define NUM_ADC_CHANNELS (INP_HAS_CALIBRATION + 2)  // Inputs + Temprature + Voltage
+#define WINDOW_SIZE 10
+#define SAMPLE_COUNT NUM_ADC_CHANNELS * WINDOW_SIZE * ADC_OVERSAMPLE_WINDOW_COUNT
 
 unsigned ADC_Read(unsigned channel);
 volatile u16 adc_array_raw[NUM_ADC_CHANNELS];
-#define WINDOW_SIZE 10
-#define SAMPLE_COUNT NUM_ADC_CHANNELS * WINDOW_SIZE * ADC_OVERSAMPLE_WINDOW_COUNT
 static volatile u16 adc_array_oversample[SAMPLE_COUNT];
+
+#if 0
+    // These are the valid ADC pins for an STM32
+    ADC_CHAN(GPIOA, GPIO0),  /* ADC123_0  */ \
+    ADC_CHAN(GPIOA, GPIO1),  /* ADC123_1  */ \
+    ADC_CHAN(GPIOA, GPIO2),  /* ADC123_2  */ \
+    ADC_CHAN(GPIOA, GPIO3),  /* ADC123_3  */ \
+    ADC_CHAN(GPIOA, GPIO4),  /* ADC12_4   */ \
+    ADC_CHAN(GPIOA, GPIO5),  /* ADC12_5   */ \
+    ADC_CHAN(GPIOA, GPIO6),  /* ADC12_6   */ \
+    ADC_CHAN(GPIOA, GPIO7),  /* ADC12_7   */ \
+    ADC_CHAN(GPIOB, GPIO0),  /* ADC12_8   */ \
+    ADC_CHAN(GPIOB, GPIO1),  /* ADC12_9   */ \
+    ADC_CHAN(GPIOF, GPIO6),  /* ADC3_4    */ \
+    ADC_CHAN(GPIOF, GPIO7),  /* ADC3_5    */ \
+    ADC_CHAN(GPIOF, GPIO8),  /* ADC3_6    */ \
+    ADC_CHAN(GPIOF, GPIO9),  /* ADC3_7    */ \
+    ADC_CHAN(GPIOF, GPIO10), /* ADC3_8    */ \
+    ADC_CHAN(GPIOC, GPIO0),  /* ADC123_10 */ \
+    ADC_CHAN(GPIOC, GPIO1),  /* ADC123_11 */ \
+    ADC_CHAN(GPIOC, GPIO2),  /* ADC123_12 */ \
+    ADC_CHAN(GPIOC, GPIO3),  /* ADC123_13 */ \
+    ADC_CHAN(GPIOC, GPIO4),  /* ADC12_14  */ \
+    ADC_CHAN(GPIOC, GPIO5),  /* ADC12_15  */ \
+    ADC_CHAN(0, 16),       /* TEMPERATURE */ \
+    // End
+#endif
+
+#define ADC_CHAN(...) ADC_PIN_TO_CHAN(__VA_ARGS__)
+static const u8 adc_chan_sel[NUM_ADC_CHANNELS] = ADC_CHANNELS;
+#undef ADC_CHAN
+
+
 void ADC_Init(void)
 {
+    #define ADC_CHAN(x, y) (x ? get_rcc_from_port(x) : 0)
+    const uint32_t adc_rcc[NUM_ADC_CHANNELS] = ADC_CHANNELS;
+    #undef ADC_CHAN
+    #define ADC_CHAN(...) {__VA_ARGS__}
+    const struct mcu_pin adc_pins[NUM_ADC_CHANNELS] = ADC_CHANNELS;
+    #undef ADC_CHAN
+    for (unsigned i = 0; i < NUM_ADC_CHANNELS; i++) {
+        if (!HAS_PIN(adc_pins[i]))
+            continue;
+        rcc_periph_clock_enable(adc_rcc[i]);
+        GPIO_setup_input(adc_pins[i], ITYPE_ANALOG);
+    }
+
     rcc_periph_clock_enable(get_rcc_from_port(ADC_CFG.adc));
     adc_power_off(ADC_CFG.adc);
     ADC_reset(ADC_CFG.adc);

src/target/drivers/mcu/stm32/gpio.h
@@ -64,6 +64,7 @@ struct dma_config {
 
 #define NULL_PIN ((struct mcu_pin){0, 0})
 #define HAS_PIN(x) (x.pin)
+#define TEMPERATURE_ADC 0, 0
 
 
 #if defined(STM32F1)

src/target/tx/devo/devo10/channels.c
@@ -23,18 +23,9 @@ const u8 adc_chan_sel[NUM_ADC_CHANNELS] = {13, 12, 11, 15, 10, 4, 16, 14};
 
 void CHAN_Init()
 {
-    rcc_peripheral_enable_clock(&RCC_APB2ENR, RCC_APB2ENR_IOPCEN);
     ADC_Init();
 
-    /* configure channels for analog */
-    gpio_set_mode(GPIOC, GPIO_MODE_INPUT, GPIO_CNF_INPUT_ANALOG, GPIO0);
-    gpio_set_mode(GPIOC, GPIO_MODE_INPUT, GPIO_CNF_INPUT_ANALOG, GPIO1);
-    gpio_set_mode(GPIOC, GPIO_MODE_INPUT, GPIO_CNF_INPUT_ANALOG, GPIO2);
-    gpio_set_mode(GPIOC, GPIO_MODE_INPUT, GPIO_CNF_INPUT_ANALOG, GPIO3);
-    gpio_set_mode(GPIOC, GPIO_MODE_INPUT, GPIO_CNF_INPUT_ANALOG, GPIO5);
-    gpio_set_mode(GPIOA, GPIO_MODE_INPUT, GPIO_CNF_INPUT_ANALOG, GPIO4);
-    /* Enable Voltage measurement */
-    gpio_set_mode(GPIOC, GPIO_MODE_INPUT, GPIO_CNF_INPUT_ANALOG, GPIO4);
+    rcc_peripheral_enable_clock(&RCC_APB2ENR, RCC_APB2ENR_IOPCEN);
 
     /* configure switches for digital I/O */
     gpio_set_mode(GPIOC, GPIO_MODE_INPUT, GPIO_CNF_INPUT_PULL_UPDOWN,

src/target/tx/devo/devo12/channels.c
@@ -28,23 +28,6 @@ void CHAN_Init()
     rcc_peripheral_enable_clock(&RCC_APB2ENR, RCC_APB2ENR_IOPFEN);
     ADC_Init();
 
-    /* Enable Voltage measurement */
-    gpio_set_mode(GPIOA, GPIO_MODE_INPUT, GPIO_CNF_INPUT_ANALOG, GPIO3); //ADC123_3
-
-    /* configure channels for analog */
-    gpio_set_mode(GPIOA, GPIO_MODE_INPUT, GPIO_CNF_INPUT_ANALOG, GPIO2); //ADC123_2
-
-    gpio_set_mode(GPIOC, GPIO_MODE_INPUT, GPIO_CNF_INPUT_ANALOG, GPIO0); //ADC123_10
-    gpio_set_mode(GPIOC, GPIO_MODE_INPUT, GPIO_CNF_INPUT_ANALOG, GPIO1); //ADC123_11
-    gpio_set_mode(GPIOC, GPIO_MODE_INPUT, GPIO_CNF_INPUT_ANALOG, GPIO2); //ADC123_12
-    gpio_set_mode(GPIOC, GPIO_MODE_INPUT, GPIO_CNF_INPUT_ANALOG, GPIO3); //ADC123_13
-
-    gpio_set_mode(GPIOF, GPIO_MODE_INPUT, GPIO_CNF_INPUT_ANALOG, GPIO6); //ADC3_4
-    gpio_set_mode(GPIOF, GPIO_MODE_INPUT, GPIO_CNF_INPUT_ANALOG, GPIO7); //ADC3_5
-    gpio_set_mode(GPIOF, GPIO_MODE_INPUT, GPIO_CNF_INPUT_ANALOG, GPIO8); //ADC3_6
-    gpio_set_mode(GPIOF, GPIO_MODE_INPUT, GPIO_CNF_INPUT_ANALOG, GPIO9); //ADC3_7
-    gpio_set_mode(GPIOF, GPIO_MODE_INPUT, GPIO_CNF_INPUT_ANALOG, GPIO10); //ADC3_8
-
     /* configure switches for digital I/O */
     //gpio_set_mode(GPIOA, GPIO_MODE_INPUT, GPIO_CNF_INPUT_PULL_UPDOWN, GPIO15);
     //gpio_set(GPIOA, GPIO15);

src/target/tx/devo/devo6/channels.c
@@ -25,21 +25,12 @@
                    | (1 << INP_SWB0) | (1 << INP_SWB1))
 
 
-const u8 adc_chan_sel[NUM_ADC_CHANNELS] = {13, 11, 10, 12, 16, 14};
-
 void CHAN_Init()
 {
-    rcc_peripheral_enable_clock(&RCC_APB2ENR, RCC_APB2ENR_IOPCEN);
     ADC_Init();
 
-    /* configure channels for analog */
-    gpio_set_mode(GPIOC, GPIO_MODE_INPUT, GPIO_CNF_INPUT_ANALOG, GPIO0);
-    gpio_set_mode(GPIOC, GPIO_MODE_INPUT, GPIO_CNF_INPUT_ANALOG, GPIO1);
-    gpio_set_mode(GPIOC, GPIO_MODE_INPUT, GPIO_CNF_INPUT_ANALOG, GPIO2);
-    gpio_set_mode(GPIOC, GPIO_MODE_INPUT, GPIO_CNF_INPUT_ANALOG, GPIO3);
-    /* Enable Voltage measurement */
-    gpio_set_mode(GPIOC, GPIO_MODE_INPUT, GPIO_CNF_INPUT_ANALOG, GPIO4);
 
+    rcc_peripheral_enable_clock(&RCC_APB2ENR, RCC_APB2ENR_IOPCEN);
     /* configure switches for digital I/O */
     gpio_set_mode(GPIOC, GPIO_MODE_INPUT, GPIO_CNF_INPUT_PULL_UPDOWN,
                    GPIO6 | GPIO7 | GPIO8 | GPIO9

src/target/tx/devo/devo7e-256/channels.c
@@ -87,30 +87,12 @@ enum {
 #define POT_2  ((1 << INP_AUX4) | (1 << INP_AUX5))
 #define POT_1  (1 << INP_AUX4)
 
-#if HAS_EXTRA_POTS
-  const u8 adc_chan_sel[NUM_ADC_CHANNELS] = {10, 12, 13, 11, 0, 4, 16, 14};
-#else
-  const u8 adc_chan_sel[NUM_ADC_CHANNELS] = {10, 12, 13, 11, 16, 14};
-#endif
-
 extern u32 global_extra_switches;
 void CHAN_Init()
 {
-    rcc_peripheral_enable_clock(&RCC_APB2ENR, RCC_APB2ENR_IOPCEN);
-    #if HAS_EXTRA_POTS
-      rcc_peripheral_enable_clock(&RCC_APB2ENR, RCC_APB2ENR_IOPAEN);
-    #endif
     ADC_Init();
 
-    /* configure channels for analog */
-    gpio_set_mode(GPIOC, GPIO_MODE_INPUT, GPIO_CNF_INPUT_ANALOG, GPIO0);
-    gpio_set_mode(GPIOC, GPIO_MODE_INPUT, GPIO_CNF_INPUT_ANALOG, GPIO1);
-    gpio_set_mode(GPIOC, GPIO_MODE_INPUT, GPIO_CNF_INPUT_ANALOG, GPIO2);
-    gpio_set_mode(GPIOC, GPIO_MODE_INPUT, GPIO_CNF_INPUT_ANALOG, GPIO3);
-    #if HAS_EXTRA_POTS
-      gpio_set_mode(GPIOA, GPIO_MODE_INPUT, GPIO_CNF_INPUT_ANALOG, GPIO0);
-      gpio_set_mode(GPIOA, GPIO_MODE_INPUT, GPIO_CNF_INPUT_ANALOG, GPIO4);
-    #endif
+    rcc_peripheral_enable_clock(&RCC_APB2ENR, RCC_APB2ENR_IOPCEN);
     /* Enable Voltage measurement */
     gpio_set_mode(GPIOC, GPIO_MODE_INPUT, GPIO_CNF_INPUT_ANALOG, GPIO4);
 

src/target/tx/devo/devo7e/channels.c
@@ -26,21 +26,12 @@
 #define SWITCH_NONE ((1 << INP_SWA0) | (1 << INP_SWA1) | (1 << INP_SWA2) \
                    | (1 << INP_SWB0) | (1 << INP_SWB1) | (1 << INP_SWB2))
 
-const u8 adc_chan_sel[NUM_ADC_CHANNELS] = {10, 12, 13, 11, 16, 14};
 extern u32 global_extra_switches;
 void CHAN_Init()
 {
-    rcc_peripheral_enable_clock(&RCC_APB2ENR, RCC_APB2ENR_IOPCEN);
     ADC_Init();
 
-    /* configure channels for analog */
-    gpio_set_mode(GPIOC, GPIO_MODE_INPUT, GPIO_CNF_INPUT_ANALOG, GPIO0);
-    gpio_set_mode(GPIOC, GPIO_MODE_INPUT, GPIO_CNF_INPUT_ANALOG, GPIO1);
-    gpio_set_mode(GPIOC, GPIO_MODE_INPUT, GPIO_CNF_INPUT_ANALOG, GPIO2);
-    gpio_set_mode(GPIOC, GPIO_MODE_INPUT, GPIO_CNF_INPUT_ANALOG, GPIO3);
-    /* Enable Voltage measurement */
-    gpio_set_mode(GPIOC, GPIO_MODE_INPUT, GPIO_CNF_INPUT_ANALOG, GPIO4);
-
+    rcc_peripheral_enable_clock(&RCC_APB2ENR, RCC_APB2ENR_IOPCEN);
     /* configure switches for digital I/O */
     gpio_set_mode(GPIOC, GPIO_MODE_INPUT, GPIO_CNF_INPUT_PULL_UPDOWN,
                    GPIO10 | GPIO11);

src/target/tx/devo/devo8/channels.c
@@ -19,21 +19,11 @@
 #include "config/tx.h"
 #include "../common/devo.h"
 
-const u8 adc_chan_sel[NUM_ADC_CHANNELS] = {13, 11, 10, 12, 16, 14};
-
 void CHAN_Init()
 {
-    rcc_peripheral_enable_clock(&RCC_APB2ENR, RCC_APB2ENR_IOPCEN);
     ADC_Init();
 
-    /* configure channels for analog */
-    gpio_set_mode(GPIOC, GPIO_MODE_INPUT, GPIO_CNF_INPUT_ANALOG, GPIO0);
-    gpio_set_mode(GPIOC, GPIO_MODE_INPUT, GPIO_CNF_INPUT_ANALOG, GPIO1);
-    gpio_set_mode(GPIOC, GPIO_MODE_INPUT, GPIO_CNF_INPUT_ANALOG, GPIO2);
-    gpio_set_mode(GPIOC, GPIO_MODE_INPUT, GPIO_CNF_INPUT_ANALOG, GPIO3);
-    /* Enable Voltage measurement */
-    gpio_set_mode(GPIOC, GPIO_MODE_INPUT, GPIO_CNF_INPUT_ANALOG, GPIO4);
-
+    rcc_peripheral_enable_clock(&RCC_APB2ENR, RCC_APB2ENR_IOPCEN);
     /* configure switches for digital I/O */
     gpio_set_mode(GPIOC, GPIO_MODE_INPUT, GPIO_CNF_INPUT_PULL_UPDOWN,
                    GPIO6 | GPIO7 | GPIO8 | GPIO9

commit df288b95a86c3f9f46a0a0830ad44be030d9f4be

Fix crash introduced by #750 (#755)
* Cosmetic update

* Fix crash due to ADC changes

* Fix at9/at10 build

src/target/drivers/input/analog/analog.c
@@ -54,10 +54,6 @@ static volatile u16 adc_array_oversample[SAMPLE_COUNT];
     // End
 #endif
 
-#define ADC_CHAN(...) ADC_PIN_TO_CHAN(__VA_ARGS__)
-static const u8 adc_chan_sel[NUM_ADC_CHANNELS] = ADC_CHANNELS;
-#undef ADC_CHAN
-
 
 void ADC_Init(void)
 {
@@ -67,13 +63,15 @@ void ADC_Init(void)
     #define ADC_CHAN(...) {__VA_ARGS__}
     const struct mcu_pin adc_pins[NUM_ADC_CHANNELS] = ADC_CHANNELS;
     #undef ADC_CHAN
+    #define ADC_CHAN(...) ADC_PIN_TO_CHAN(__VA_ARGS__)
+    static const u8 adc_chan_sel[NUM_ADC_CHANNELS] = ADC_CHANNELS;
+    #undef ADC_CHAN
     for (unsigned i = 0; i < NUM_ADC_CHANNELS; i++) {
         if (!HAS_PIN(adc_pins[i]))
             continue;
         rcc_periph_clock_enable(adc_rcc[i]);
         GPIO_setup_input(adc_pins[i], ITYPE_ANALOG);
     }
-
     rcc_periph_clock_enable(get_rcc_from_port(ADC_CFG.adc));
     adc_power_off(ADC_CFG.adc);
     ADC_reset(ADC_CFG.adc);
@@ -215,3 +213,13 @@ void ADC_ScanChannels()
         }
     }
 }
+
+/* Return milivolts */
+unsigned PWR_ReadVoltage(void)
+{
+    u32 v = adc_array_raw[NUM_ADC_CHANNELS-1];
+    /* Multily the above by 1000 to get milivolts */
+    v = v * VOLTAGE_NUMERATOR / 100 + VOLTAGE_OFFSET;
+    return v;
+}
+

src/target/drivers/mcu/stm32/gpio.h
@@ -63,8 +63,7 @@ struct dma_config {
 };
 
 #define NULL_PIN ((struct mcu_pin){0, 0})
-#define HAS_PIN(x) (x.pin)
-#define TEMPERATURE_ADC 0, 0
+#define HAS_PIN(x) (x.port)
 
 
 #if defined(STM32F1)

src/target/tx/devo/common/devo.h
@@ -21,7 +21,6 @@ u32 SOUND_Callback();
 extern void PROTO_Stubs(int);
 // ADC defines
 #define NUM_ADC_CHANNELS (INP_HAS_CALIBRATION + 2) //Inputs + Temprature + Voltage
-extern const u8 adc_chan_sel[NUM_ADC_CHANNELS];
 extern volatile u16 adc_array_raw[NUM_ADC_CHANNELS];
 void ADC_Filter();
 

src/target/tx/devo/common/power.c
@@ -78,15 +78,6 @@ void PWR_Sleep()
     //asm("wfi");
 }
 
-/* Return milivolts */
-unsigned PWR_ReadVoltage(void)
-{
-    u32 v = adc_array_raw[NUM_ADC_CHANNELS-1];
-    /* Multily the above by 1000 to get milivolts */
-    v = v * VOLTAGE_NUMERATOR / 100 + VOLTAGE_OFFSET;
-    return v;
-}
-
 void PWR_JumpToProgrammer()
 {
     scb_reset_system();

src/target/tx/devo/devo10/channels.c
@@ -19,8 +19,6 @@
 #include "config/tx.h"
 #include "../common/devo.h"
 
-const u8 adc_chan_sel[NUM_ADC_CHANNELS] = {13, 12, 11, 15, 10, 4, 16, 14};
-
 void CHAN_Init()
 {
     ADC_Init();

src/target/tx/devo/devo12/channels.c
@@ -19,8 +19,6 @@
 #include "config/tx.h"
 #include "../common/devo.h"
 
-const u8 adc_chan_sel[NUM_ADC_CHANNELS] = {2, 11, 10, 8, 5, 12, 6, 13, 4, 7, 16, 3};
-
 void CHAN_Init()
 {
     rcc_peripheral_enable_clock(&RCC_APB2ENR, RCC_APB2ENR_IOPCEN);

src/target/tx/radiolink/at10/at10_power.c
@@ -44,12 +44,3 @@ void PWR_Sleep()
 {
     // asm("wfi");
 }
-
-/* Return milivolts */
-unsigned PWR_ReadVoltage(void)
-{
-    u32 v = adc_array_raw[NUM_ADC_CHANNELS-1];
-    /* Multily the above by 1000 to get milivolts */
-    v = v * VOLTAGE_NUMERATOR / 100 + VOLTAGE_OFFSET;
-    return v;
-}

src/target/tx/radiolink/at9/at9_power.c
@@ -51,12 +51,3 @@ void PWR_Sleep()
 {
     //asm("wfi");
 }
-
-/* Return milivolts */
-unsigned PWR_ReadVoltage(void)
-{
-    u32 v = adc_array_raw[NUM_ADC_CHANNELS-1];
-    /* Multily the above by 1000 to get milivolts */
-    v = v * VOLTAGE_NUMERATOR / 100 + VOLTAGE_OFFSET;
-    return v;
-}