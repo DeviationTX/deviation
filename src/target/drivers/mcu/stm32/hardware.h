a28d980170e7d22d3b13ee85ea09255446a747ac
PhracturedBlue <rc2012@pblue.org>
2019-02-23 14:26:11 -0800

    Fix emu build on windows

--- src/target/drivers/mcu/stm32/hardware.h
+++ src/target/drivers/mcu/stm32/hardware.h
@@ -2,15 +2,7 @@
 #define _DTX_STM32_HARDWARE_DEFAULT_H_
 
 // Add additional callbacks
-#ifdef TARGET_PRIORITY
-    // A trick to concatenate macro-definitions from https://stackoverflow.com/questions/4550075/can-i-append-to-a-preprocessor-macro
-    _Pragma("push_macro(\"TARGET_PRIORITY\")")
-    #undef TARGET_PRIORITY
-    #define TARGET_PRIORITY \
-        _Pragma("pop_macro(\"TARGET_PRIORITY\")") TARGET_PRIORITY, \
-        TIMER_SOUND
-    #undef old_TARGET_PRIORITY
-#else
+#ifndef TARGET_PRIORITY
     #define TARGET_PRIORITY TIMER_SOUND
 #endif
 
