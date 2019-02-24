322fc65181d7e2576df8cdfc8a9a0768688d99e9
PhracturedBlue <rc2012@pblue.org>
2019-02-24 04:09:58 -0800

    Fix radiolink build

--- src/target/tx/radiolink/common/common_at.h
+++ src/target/tx/radiolink/common/common_at.h
@@ -8,9 +8,14 @@
     #include "enable_petit_fat.h"
 #endif
 
-#define FLASHTYPE FLASHTYPE_MCU
+#if defined(HAS_4IN1_FLASH) && HAS_4IN1_FLASH
+    #define FLASHTYPE FLASHTYPE_SPI
+    #define HAS_FLASH_DETECT
+#else
+    #define FLASHTYPE FLASHTYPE_MCU
+#endif
 
-#define USE_4BUTTON_MODE 1
+#define USE_4BUTTON_MODE    1
 #define HAS_HARD_POWER_OFF  1
 
 #include "hardware.h"
