c9421d0cdddefe8dcdd140883b7c0fb8163ffc4f
Howard Su <howard0su@gmail.com>
2019-02-27 07:25:39 +0800

    Update HID driver to match original implementation

--- src/target/drivers/usb/devo_hid.c
+++ src/target/drivers/usb/devo_hid.c
@@ -5,44 +5,43 @@
 #include "devo_usb.h"
 
 static const uint8_t hid_report_descriptor[] = {
-    0x05, 0x01, /* USAGE_PAGE (Generic Desktop)         */
-    0x09, 0x02, /* USAGE (Mouse)                        */
-    0xa1, 0x01, /* COLLECTION (Application)             */
-    0x09, 0x01, /*   USAGE (Pointer)                    */
-    0xa1, 0x00, /*   COLLECTION (Physical)              */
-    0x05, 0x09, /*     USAGE_PAGE (Button)              */
-    0x19, 0x01, /*     USAGE_MINIMUM (Button 1)         */
-    0x29, 0x03, /*     USAGE_MAXIMUM (Button 3)         */
-    0x15, 0x00, /*     LOGICAL_MINIMUM (0)              */
-    0x25, 0x01, /*     LOGICAL_MAXIMUM (1)              */
-    0x95, 0x03, /*     REPORT_COUNT (3)                 */
-    0x75, 0x01, /*     REPORT_SIZE (1)                  */
-    0x81, 0x02, /*     INPUT (Data,Var,Abs)             */
-    0x95, 0x01, /*     REPORT_COUNT (1)                 */
-    0x75, 0x05, /*     REPORT_SIZE (5)                  */
-    0x81, 0x01, /*     INPUT (Cnst,Ary,Abs)             */
-    0x05, 0x01, /*     USAGE_PAGE (Generic Desktop)     */
-    0x09, 0x30, /*     USAGE (X)                        */
-    0x09, 0x31, /*     USAGE (Y)                        */
-    0x09, 0x38, /*     USAGE (Wheel)                    */
-    0x15, 0x81, /*     LOGICAL_MINIMUM (-127)           */
-    0x25, 0x7f, /*     LOGICAL_MAXIMUM (127)            */
-    0x75, 0x08, /*     REPORT_SIZE (8)                  */
-    0x95, 0x03, /*     REPORT_COUNT (3)                 */
-    0x81, 0x06, /*     INPUT (Data,Var,Rel)             */
-    0xc0,       /*   END_COLLECTION                     */
-    0x09, 0x3c, /*   USAGE (Motion Wakeup)              */
-    0x05, 0xff, /*   USAGE_PAGE (Vendor Defined Page 1) */
-    0x09, 0x01, /*   USAGE (Vendor Usage 1)             */
-    0x15, 0x00, /*   LOGICAL_MINIMUM (0)                */
-    0x25, 0x01, /*   LOGICAL_MAXIMUM (1)                */
-    0x75, 0x01, /*   REPORT_SIZE (1)                    */
-    0x95, 0x02, /*   REPORT_COUNT (2)                   */
-    0xb1, 0x22, /*   FEATURE (Data,Var,Abs,NPrf)        */
-    0x75, 0x06, /*   REPORT_SIZE (6)                    */
-    0x95, 0x01, /*   REPORT_COUNT (1)                   */
-    0xb1, 0x01, /*   FEATURE (Cnst,Ary,Abs)             */
-    0xc0        /* END_COLLECTION                       */
+    0x05, 0x01,                    // USAGE_PAGE (Generic Desktop)
+    0x15, 0x81,                    // LOGICAL_MINIMUM (0)
+    0x25, 0x7f,                    // LOGICAL_MAXIMUM (255)
+    0x75, 0x08,                    // REPORT_SIZE (8)
+    0x09, 0x04,                    // USAGE (Joystick)
+    0xa1, 0x01,                    // COLLECTION (Application)
+    0x09, 0x01,                    //   USAGE (Pointer)
+    0xa1, 0x00,                    //   COLLECTION (Physical)
+    0x09, 0x30,                    //     USAGE (X)
+    0x09, 0x31,                    //     USAGE (Y)
+    0x95, 0x02,                    //     REPORT_COUNT (2)
+    0x81, 0x82,                    //     INPUT (Data,Var,Abs,Vol)
+    0xc0,                          //   END_COLLECTION
+    0xa1, 0x00,                    //   COLLECTION (Physical)
+    0x09, 0x32,                    //     USAGE (Z)
+    0x09, 0x33,                    //     USAGE (Rx)
+    0x95, 0x02,                    //     REPORT_COUNT (2)
+    0x81, 0x82,                    //     INPUT (Data,Var,Abs,Vol)
+    0xc0,                          //   END_COLLECTION
+    0x09, 0x34,                    //   USAGE (Ry)
+    0x09, 0x35,                    //   USAGE (Rz)
+    0x09, 0x36,                    //   USAGE (Slider)
+    0x09, 0x37,                    //   USAGE (Dial)
+    0x95, 0x04,                    //   REPORT_COUNT (4)
+    0x81, 0x82,                    //   INPUT (Data,Var,Abs,Vol)
+    0x05, 0x09,                    //   USAGE_PAGE (Button)
+    0x19, 0x01,                    //   USAGE_MINIMUM (Button 1)
+    0x29, 0x04,                    //   USAGE_MAXIMUM (Button 4)
+    0x15, 0x00,                    //   LOGICAL_MINIMUM (0)
+    0x25, 0x01,                    //   LOGICAL_MAXIMUM (1)
+    0x95, 0x04,                    //   REPORT_COUNT (4)
+    0x75, 0x01,                    //   REPORT_SIZE (1)
+    0x81, 0x02,                    //   INPUT (Data,Var,Abs)
+    0x95, 0x01,                    //   REPORT_COUNT (1)
+    0x75, 0x04,                    //   REPORT_SIZE (4)
+    0x81, 0x03,                    //   INPUT (Cnst,Var,Abs)
+    0xc0                           // END_COLLECTION
 };
 
 static const struct {
@@ -55,7 +54,7 @@ static const struct {
     .hid_descriptor = {
         .bLength = sizeof(hid_function),
         .bDescriptorType = USB_DT_HID,
-        .bcdHID = 0x0100,
+        .bcdHID = 0x0000,
         .bCountryCode = 0,
         .bNumDescriptors = 1,
     },
@@ -70,7 +69,7 @@ const struct usb_endpoint_descriptor hid_endpoint = {
     .bDescriptorType = USB_DT_ENDPOINT,
     .bEndpointAddress = 0x81,
     .bmAttributes = USB_ENDPOINT_ATTR_INTERRUPT,
-    .wMaxPacketSize = 4,
+    .wMaxPacketSize = 9,
     .bInterval = 0x20,
 };
 
@@ -81,8 +80,8 @@ static const struct usb_interface_descriptor hid_iface = {
     .bAlternateSetting = 0,
     .bNumEndpoints = 1,
     .bInterfaceClass = USB_CLASS_HID,
-    .bInterfaceSubClass = 1, /* boot */
-    .bInterfaceProtocol = 2, /* mouse */
+    .bInterfaceSubClass = 0,  // 1=BOOT, 0=no boot
+    .bInterfaceProtocol = 0,  // 0=none, 1=keyboard, 2=mouse
     .iInterface = 0,
     .endpoint = &hid_endpoint,
     .extra = &hid_function,
@@ -101,17 +100,16 @@ static const struct usb_config_descriptor hid_config_descr = {
     .bNumInterfaces = 1,
     .bConfigurationValue = 1,
     .iConfiguration = 0,
-    .bmAttributes = 0xC0,
+    .bmAttributes = 0xE0,
     .bMaxPower = 0x32,
     .interface = &hid_ifaces,
 };
 
-
 static void HID_Init()
 {
     usbd_dev = usbd_init(&st_usbfs_v1_usb_driver, &dev_descr, &hid_config_descr,
-      usb_strings, USB_STRING_COUNT,
-      usbd_control_buffer, sizeof(usbd_control_buffer));
+        usb_strings, USB_STRING_COUNT,
+        usbd_control_buffer, sizeof(usbd_control_buffer));
 }
 
 void HID_Write(s8 *packet, u8 num_channels)
