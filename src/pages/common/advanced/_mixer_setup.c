9ec21c0b3866022d61b892ecd29eb9464801224c
Howard Su <howard0su@gmail.com>
2019-03-04 20:54:27 +0800

    make CURVE_GetName return non-loc strings

--- src/pages/common/advanced/_mixer_setup.c
+++ src/pages/common/advanced/_mixer_setup.c
@@ -495,7 +495,7 @@ static const char *set_curvename_cb(guiObject_t *obj, int dir, void *data)
         MIXPAGE_RedrawGraphs();
     }
     GUI_TextSelectEnablePress((guiTextSelect_t *)obj, type > CURVE_FIXED);
-    return CURVE_GetName(tempstring, &mix->curve);
+    return _tr(CURVE_GetName(tempstring, &mix->curve));
 }
 
 void sourceselect_cb(guiObject_t *obj, void *data)
