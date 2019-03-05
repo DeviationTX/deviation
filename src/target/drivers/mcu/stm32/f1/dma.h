7f9fe5faf2973a5f56f02ad088ea4e49c37042be
PhracturedBlue <rc2012@pblue.org>
2019-03-04 21:20:29 -0800

    Make ADC intialization more generic to handle STM32F2 (#800)

--- src/target/drivers/mcu/stm32/f1/dma.h
+++ src/target/drivers/mcu/stm32/f1/dma.h
@@ -10,6 +10,8 @@
 #define DMA_SxCR_MSIZE_8BIT DMA_CCR_MSIZE_8BIT
 #define DMA_SxCR_PSIZE_8BIT DMA_CCR_PSIZE_8BIT
 
+#define DMA_SxCR_PL_VERY_HIGH 0x01  // Unused on F1
+
 inline static void DMA_stream_reset(struct dma_config dma)
 {
     dma_channel_reset(dma.dma, dma.stream);
@@ -21,18 +23,23 @@ inline static void DMA_channel_select(struct dma_config dma)
     (void)dma;
 }
 
-inline static void DMA_set_dma_flow_control(uint32_t dma, uint32_t stream)
+inline static void DMA_set_dma_flow_control(struct dma_config dma)
 {
     // Not implemented on STM32F1
     (void)dma;
-    (void)stream;
 }
 
-inline static void DMA_enable_direct_mode(uint32_t dma, uint32_t stream)
+inline static void DMA_set_priority(struct dma_config dma, uint32_t priority)
 {
     // Not implemented on STM32F1
     (void)dma;
-    (void)stream;
+    (void)priority;
+}
+
+inline static void DMA_enable_direct_mode(struct dma_config dma)
+{
+    // Not implemented on STM32F1
+    (void)dma;
 }
 
 inline static void DMA_set_transfer_mode(struct dma_config dma, uint32_t direction)
@@ -44,11 +51,10 @@ inline static void DMA_set_transfer_mode(struct dma_config dma, uint32_t directi
     }
 }
 
-inline static void DMA_disable_double_buffer_mode(uint32_t dma, uint32_t stream)
+inline static void DMA_disable_double_buffer_mode(struct dma_config dma)
 {
     // Not implemented on STM32F1
     (void)dma;
-    (void)stream;
 }
 
 inline static void DMA_enable_stream(struct dma_config dma)
