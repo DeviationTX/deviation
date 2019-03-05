320a6713ce1d2864717fda18a55367b417c5ee33
PhracturedBlue <rc2012@pblue.org>
2019-03-04 20:16:24 -0800

    Enable LCD to init SPI when no SPI-Flash is present (#801)

--- src/target/drivers/display/spi/128x64x1_nt7538.c
+++ src/target/drivers/display/spi/128x64x1_nt7538.c
@@ -14,10 +14,10 @@
 */
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/rcc.h>
-#include <libopencm3/stm32/spi.h>
 #include "common.h"
 #include "gui/gui.h"
 #include "target/drivers/mcu/stm32/rcc.h"
+#include "target/drivers/mcu/stm32/spi.h"
 
 #define CS_HI() GPIO_pin_set(LCD_SPI.csn)
 #define CS_LO() GPIO_pin_clear(LCD_SPI.csn)
@@ -87,6 +87,9 @@ void LCD_Init()
 {
     //Initialization is mostly done in SPI Flash
     //Setup CS as B.0 Data/Control = C.5
+#ifndef FLASH_SPI_CFG
+    _spi_init(LCD_SPI_CFG);
+#endif
     rcc_periph_clock_enable(get_rcc_from_pin(LCD_SPI.csn));
     rcc_periph_clock_enable(get_rcc_from_pin(LCD_SPI_MODE));
     GPIO_setup_output(LCD_SPI.csn, OTYPE_PUSHPULL);
