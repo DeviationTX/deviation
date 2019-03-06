adf0f32e8770b135c56a27184324ad4e7e00560a
PhracturedBlue <rc2012@pblue.org>
2019-03-06 04:59:24 -0800

    Enable AFIO and ensure the CYRF6936 reset pin is configured

--- src/target/drivers/mcu/stm32/f1/jtag.h
+++ src/target/drivers/mcu/stm32/f1/jtag.h
@@ -1,9 +1,11 @@
 #ifndef _DTX_STM32F1_JTAG_H_
 #define _DTX_STM32F1_JTAG_H_
 #include "common.h"
+#include "../rcc.h"
 
 static inline void DisableJTAG()
 {
+    rcc_periph_clock_enable(RCC_AFIO);
     AFIO_MAPR = (AFIO_MAPR & ~AFIO_MAPR_SWJ_MASK) | AFIO_MAPR_SWJ_CFG_JTAG_OFF_SW_OFF;
     gpio_set_mode(GPIO_BANK_JTMS_SWDIO, GPIO_MODE_OUTPUT_50_MHZ,
                   GPIO_CNF_OUTPUT_PUSHPULL, GPIO_JTMS_SWDIO);
