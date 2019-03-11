11b861c5fa3afa97698777d303312176dcdc0c35
PhracturedBlue <rc2012@pblue.org>
2019-03-11 09:18:16 -0700

    Move analog channel raw data code into analog.c

--- src/target/tx/devo/devo7e-256/hardware.h
+++ src/target/tx/devo/devo7e-256/hardware.h
@@ -5,15 +5,26 @@
 #include "../common/hardware_t8sg_buttonmatrix.h"
 
 // Analog inputs
-#define ADC_CHANNELS { \
-    ADC_CHAN(GPIOC, GPIO2),  /* ADC123_12 - INP_AIL */ \
-    ADC_CHAN(GPIOC, GPIO1),  /* ADC123_11 - INP_ELE */ \
-    ADC_CHAN(GPIOC, GPIO0),  /* ADC123_10 - INP_THR */ \
-    ADC_CHAN(GPIOC, GPIO3),  /* ADC123_13 - INP_RUD */ \
-    ADC_CHAN(GPIOA, GPIO0),  /* ADC123_0  - INP_AUX4 */ \
-    ADC_CHAN(GPIOA, GPIO4),  /* ADC12_4   - INP_AUX5 */ \
-    ADC_CHAN(0, 16),       /* TEMPERATURE */ \
-    ADC_CHAN(GPIOC, GPIO4),  /* ADC12_14  */ \
-    }
+#if HAS_EXTRA_POTS
+    #define ADC_CHANNELS { \
+        ADC_CHAN(GPIOC, GPIO2),  /* ADC123_12 - INP_AIL */ \
+        ADC_CHAN(GPIOC, GPIO1),  /* ADC123_11 - INP_ELE */ \
+        ADC_CHAN(GPIOC, GPIO0),  /* ADC123_10 - INP_THR */ \
+        ADC_CHAN(GPIOC, GPIO3),  /* ADC123_13 - INP_RUD */ \
+        ADC_CHAN(GPIOA, GPIO0),  /* ADC123_0  */ \
+        ADC_CHAN(GPIOA, GPIO4),  /* ADC12_4   */ \
+        ADC_CHAN(0, 16),       /* TEMPERATURE */ \
+        ADC_CHAN(GPIOC, GPIO4),  /* ADC12_14  */ \
+        }
+#else
+    #define ADC_CHANNELS { \
+        ADC_CHAN(GPIOC, GPIO0),  /* ADC123_10 */ \
+        ADC_CHAN(GPIOC, GPIO2),  /* ADC123_12 */ \
+        ADC_CHAN(GPIOC, GPIO3),  /* ADC123_13 */ \
+        ADC_CHAN(GPIOC, GPIO1),  /* ADC123_11 */ \
+        ADC_CHAN(0, 16),       /* TEMPERATURE */ \
+        ADC_CHAN(GPIOC, GPIO4),  /* ADC12_14  */ \
+        }
+#endif  // HAS_EXTRA_POTS
 
 #endif  // _DEVO7E256_HARDWARE_H_
