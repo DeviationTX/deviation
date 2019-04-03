1a2c6327aef7e25d29a5ad287d4b4270ffe6cdda
PhracturedBlue <rc2012@pblue.org>
2019-04-02 20:58:39 -0700

    Fix 3x2 switch configu for devo7e (#842)
    * Fix 3x2 switch configu for devo7e
    
    * Fixes for 2x1 switch

--- src/target/drivers/input/addon_switch/addon_btnmatrix.c
+++ src/target/drivers/input/addon_switch/addon_btnmatrix.c
@@ -33,12 +33,12 @@ void _scan_extra_switches()
     GPIO_pin_clear(EXTRA_SWITCH_COL_OD);
     u32 port = GPIO_pin_get(BUTTON_MATRIX_ROW_OD);
     GPIO_setup_input(EXTRA_SWITCH_COL_OD, ITYPE_PULLUP);
-    if (SW_ENABLED(SWITCH_3x1) && !SW_ENABLED(SWITCH_3x2)) {
-        global_extra_switches = (((~port) >> 4) & 0x04) | (((~port) >> 5) & 0x08);
-    } else if SW_ENABLED(SWITCH_2x2) {
-        global_extra_switches  = (port>>6)&0x05;
-    } else {
+    if SW_ENABLED(SWITCH_3x2) {
         global_extra_switches  = (~(port>>5))&0xf;
+    } else if (SW_ENABLED(SWITCH_3x1)) {
+        global_extra_switches = (((~port) >> 4) & 0x04) | (((~port) >> 5) & 0x08);
+    } else {
+        global_extra_switches  = (port>>6)&0x05;
     }
 }
 
