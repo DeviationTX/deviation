afe4229eceb405859186009e13a9dff5167e5173
vladislavy <vladislavy@rambler.ru>
2019-08-09 07:53:11 +0300

    Fix devof12e toggles #811

--- src/pages/common/_main_page.c
+++ src/pages/common/_main_page.c
@@ -240,6 +240,7 @@ void PAGE_MainEvent()
                 }
                 if (idx != -1) {
 #ifdef HAS_CHAR_ICONS
+                    GUI_SetHidden((guiObject_t *)&gui->elem[i], 1);
                     gui->elem[i].box.cb_data = (void *)(long)idx;
 #else
                     struct ImageMap img;
