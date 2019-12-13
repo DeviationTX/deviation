08f36e69711586ddaced9004f064107ecef981d1
vladislavy <vladislavy@rambler.ru>
2019-12-13 11:24:12 +0300

    Enable STANDARD GUI for Devo 8

--- src/target/tx/devo/devo8/target_defs.h
+++ src/target/tx/devo/devo8/target_defs.h
@@ -7,7 +7,7 @@
 #define SPIFLASH_SECTORS 1024
 
 #define HAS_LCD_TYPES       (LCDTYPE_HX8347 | LCDTYPE_ILI9341)
-#define HAS_STANDARD_GUI    0
+#define HAS_STANDARD_GUI    1
 #define HAS_ADVANCED_GUI    1
 #define HAS_PERMANENT_TIMER 1
 #define HAS_TELEMETRY       1
@@ -16,14 +16,14 @@
 #define HAS_RTC             0
 #define HAS_VIBRATINGMOTOR  OPTIONAL
 #define HAS_DATALOG         1
-#define SUPPORT_SCANNER         1
+#define SUPPORT_SCANNER     1
 #define HAS_EXTRA_SWITCHES  0
-#define HAS_EXTRA_BUTTONS  0
+#define HAS_EXTRA_BUTTONS   0
 #define HAS_MULTIMOD_SUPPORT 1
 #define HAS_VIDEO           0
 #define HAS_4IN1_FLASH      1
 #define HAS_EXTENDED_AUDIO  1
-#define HAS_AUDIO_UART     0
+#define HAS_AUDIO_UART      0
 #define HAS_MUSIC_CONFIG    1
 
 #ifdef BUILDTYPE_DEV
