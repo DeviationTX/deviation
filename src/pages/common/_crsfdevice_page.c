b4b75414a3be268a3576a941e61c238a816d39cf
hexfet <jpublic123@yahoo.com>
2023-01-27 20:08:15 -0500

    Clarify string length code.  Fix one length bug.

--- src/pages/common/_crsfdevice_page.c
+++ src/pages/common/_crsfdevice_page.c
@@ -697,9 +697,10 @@ static void add_param(u8 *buffer, u8 num_bytes) {
     parameter->type = *recv_param_ptr & 0x7f;
     if (!update) {
         parameter->hidden = *recv_param_ptr++ & 0x80;
-        parameter->name = alloc_string(strlen(recv_param_ptr)+1);
-        strlcpy(parameter->name, (const char *)recv_param_ptr, strlen(recv_param_ptr)+1);
-        recv_param_ptr += strlen(recv_param_ptr) + 1;
+        const u8 length = strlen(recv_param_ptr) + 1;
+        parameter->name = alloc_string(length);
+        strlcpy(parameter->name, (const char *)recv_param_ptr, length);
+        recv_param_ptr += length;
     } else {
         if (parameter->hidden != (*recv_param_ptr & 0x80))
             params_loaded = 0;   // if item becomes hidden others may also, so reload all params
@@ -721,16 +722,18 @@ static void add_param(u8 *buffer, u8 num_bytes) {
             parse_bytes(UINT8, &recv_param_ptr, &parameter->u.point);
             parse_bytes(FLOAT, &recv_param_ptr, &parameter->step);
         } else if (*recv_param_ptr) {
-            if (!update) parameter->s.unit = alloc_string(strlen(recv_param_ptr)+1);
-            strlcpy(parameter->s.unit, (const char *)recv_param_ptr, strlen(recv_param_ptr)+1);
+            const u8 length = strlen(recv_param_ptr) + 1;
+            if (!update) parameter->s.unit = alloc_string(length);
+            strlcpy(parameter->s.unit, (const char *)recv_param_ptr, length);
         }
         break;
 
     case TEXT_SELECTION:
         if (!update) {
-            parameter->value = alloc_string(strlen(recv_param_ptr)+1);
-            strlcpy(parameter->value, (const char *)recv_param_ptr, strlen(recv_param_ptr)+1);
-            recv_param_ptr += strlen(recv_param_ptr) + 1;
+            const u8 length = strlen(recv_param_ptr) + 1;
+            parameter->value = alloc_string(length);
+            strlcpy(parameter->value, (const char *)recv_param_ptr, length);
+            recv_param_ptr += length;
             // put null between selection options
             // find max choice string length to adjust textselectplate size
             char *start = (char *)parameter->value;
@@ -749,7 +752,7 @@ static void add_param(u8 *buffer, u8 num_bytes) {
                 // handle "Lua up arrow and down arrow - replace with u and d
                 else if (*p == 0xc0) *p = 'u';
                 else if (*p == 0xc1) *p = 'd';
-                else if (*p &  0x80) *p = ' ';
+                else if (*p &  0x80) *p = '?';
             }
             parameter->max_value = count;   // bug fix for incorrect max from device
         } else {
@@ -763,9 +766,10 @@ static void add_param(u8 *buffer, u8 num_bytes) {
 
     case INFO:
         if (!update) {
-            parameter->value = alloc_string(strlen(recv_param_ptr)+1);
-            strlcpy(parameter->value, (const char *)recv_param_ptr, strlen(recv_param_ptr+1));
-            recv_param_ptr += strlen(recv_param_ptr) + 1;
+            const u8 length = strlen(recv_param_ptr) + 1;
+            parameter->value = alloc_string(length);
+            strlcpy(parameter->value, (const char *)recv_param_ptr, length);
+            recv_param_ptr += length;
         }
         break;
 
@@ -782,8 +786,9 @@ static void add_param(u8 *buffer, u8 num_bytes) {
             if (!update) parameter->value = alloc_string(parameter->u.string_max_len+1);
             strlcpy(parameter->value, value, parameter->u.string_max_len+1);
             if (!update) {
-                parameter->default_value = alloc_string(strlen(default_value)+1);
-                strlcpy(parameter->default_value, default_value, strlen(default_value)+1);
+                const u8 length = strlen(default_value) + 1;
+                parameter->default_value = alloc_string(length);
+                strlcpy(parameter->default_value, default_value, length);
             }
         }
         break;
